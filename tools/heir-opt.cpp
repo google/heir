0a3455847acdc41ff5f66583078a85b306935034
Jeremy Kun <j2kun@users.noreply.github.com>
2023-08-02 04:37:13 -0700

    remove HEIR test dialect

--- tools/heir-opt.cpp
+++ tools/heir-opt.cpp
@@ -1,6 +1,5 @@
 #include "include/Conversion/MemrefToArith/MemrefToArith.h"
 #include "include/Dialect/EncryptedArith/IR/EncryptedArithDialect.h"
-#include "include/Dialect/HEIR/IR/HEIRDialect.h"
 #include "include/Dialect/Poly/IR/PolyDialect.h"
 #include "mlir/include/mlir/Conversion/TosaToLinalg/TosaToLinalg.h" // from @llvm-project
 #include "mlir/include/mlir/Dialect/Affine/IR/AffineOps.h" // from @llvm-project
@@ -71,7 +70,6 @@ void tosaPipelineBuilder(mlir::OpPassManager &manager) {
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   registry.insert<mlir::heir::poly::PolyDialect>();
-  registry.insert<mlir::heir::HEIRDialect>();
   registry.insert<mlir::heir::EncryptedArithDialect>();
 
   // Add expected MLIR dialects to the registry.
