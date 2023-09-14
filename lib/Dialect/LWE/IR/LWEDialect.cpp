a06ea88efb215fe31a646082edec6e587ae53d2e
Jeremy Kun <j2kun@users.noreply.github.com>
2023-09-14 16:47:50 -0700

    add RLWE coefficient-packed encoding

--- lib/Dialect/LWE/IR/LWEDialect.cpp
+++ lib/Dialect/LWE/IR/LWEDialect.cpp
@@ -1,6 +1,7 @@
 #include "include/Dialect/LWE/IR/LWEDialect.h"
 
 #include "include/Dialect/LWE/IR/LWEAttributes.h"
+#include "include/Dialect/Poly/IR/PolyTypes.h"
 #include "llvm/include/llvm/ADT/TypeSwitch.h"            // from @llvm-project
 #include "mlir/include/mlir/IR/DialectImplementation.h"  // from @llvm-project
 
@@ -46,6 +47,36 @@ LogicalResult BitFieldEncodingAttr::verifyEncoding(
   // It may be worth adding some sort of warning notification if the attribute
   // allocates no bits for noise, since this would be effectively useless for
   // FHE.
+  return success();
+}
+
+LogicalResult PolyCoefficientEncodingAttr::verifyEncoding(
+    ArrayRef<int64_t> shape, Type elementType,
+    ::llvm::function_ref<::mlir::InFlightDiagnostic()> emitError) const {
+  if (!elementType.isa<poly::PolyType>()) {
+    return emitError() << "Tensors with a poly_coefficient_encoding must have "
+                       << "`poly.poly` element type, but found " << elementType
+                       << "\n";
+  }
+
+  poly::PolyType polyType = llvm::cast<poly::PolyType>(elementType);
+  // The coefficient modulus takes the place of the plaintext bitwidth for
+  // RLWE.
+  unsigned plaintextBitwidth =
+      polyType.getRing().coefficientModulus().getBitWidth();
+  unsigned cleartextBitwidth = getCleartextBitwidth();
+  if (plaintextBitwidth < cleartextBitwidth)
+    return emitError() << "The polys in this tensor have a coefficient "
+                       << "modulus with bitwidth " << plaintextBitwidth
+                       << ", which too small to store the cleartext, "
+                       << "which has bit width " << cleartextBitwidth << "";
+
+  auto cleartextStart = getCleartextStart();
+  if (cleartextStart < 0 || cleartextStart >= plaintextBitwidth)
+    return emitError() << "Attribute's cleartext starting bit index ("
+                       << cleartextStart << ") is outside the legal range [0, "
+                       << plaintextBitwidth - 1 << "]";
+
   return success();
 }
 
--- tests/lwe/attributes.mlir
+++ tests/lwe/attributes.mlir
@@ -28,3 +28,39 @@ func.func @test_invalid_lwe_attribute() {
     %coeffs1 = tensor.from_elements %two, %two : tensor<2xi16, #encoding2>
   return
 }
+
+// -----
+
+#generator = #poly.polynomial<1 + x**1024>
+#ring = #poly.ring<cmod=65536, ideal=#generator>
+#coeff_encoding = #lwe.poly_coefficient_encoding<cleartext_start=15, cleartext_bitwidth=4>
+
+// CHECK: poly_coefficient_encoding
+func.func @test_valid_coefficient_encoding_attr(%coeffs1 : tensor<10xi16>, %coeffs2 : tensor<10xi16>) {
+  %poly1 = poly.from_tensor %coeffs1 : tensor<10xi16> -> !poly.poly<#ring>
+  %poly2 = poly.from_tensor %coeffs2 : tensor<10xi16> -> !poly.poly<#ring>
+  %rlwe_ciphertext = tensor.from_elements %poly1, %poly2 : tensor<2x!poly.poly<#ring>, #coeff_encoding>
+  return
+}
+
+// -----
+
+#coeff_encoding1 = #lwe.poly_coefficient_encoding<cleartext_start=15, cleartext_bitwidth=4>
+// expected-error@below {{must have `poly.poly` element type}}
+func.func @test_invalid_coefficient_encoding_type(%a : i16, %b: i16) {
+  %rlwe_ciphertext = tensor.from_elements %a, %b : tensor<2xi16, #coeff_encoding1>
+  return
+}
+
+// -----
+
+#generator2 = #poly.polynomial<1 + x**1024>
+#ring2 = #poly.ring<cmod=65536, ideal=#generator>
+#coeff_encoding2 = #lwe.poly_coefficient_encoding<cleartext_start=30, cleartext_bitwidth=3>
+// expected-error@below {{cleartext starting bit index (30) is outside the legal range [0, 15]}}
+func.func @test_invalid_coefficient_encoding_width(%coeffs1 : tensor<10xi16>, %coeffs2 : tensor<10xi16>) {
+  %poly1 = poly.from_tensor %coeffs1 : tensor<10xi16> -> !poly.poly<#ring2>
+  %poly2 = poly.from_tensor %coeffs2 : tensor<10xi16> -> !poly.poly<#ring2>
+  %rlwe_ciphertext = tensor.from_elements %poly1, %poly2 : tensor<2x!poly.poly<#ring2>, #coeff_encoding2>
+  return
+}
