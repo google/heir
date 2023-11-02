3ff8a72615c8095c83f5103facf803275967792b
Jeremy Kun <j2kun@users.noreply.github.com>
2023-11-02 08:07:19 -0700

    Add encrypted int types

--- include/Dialect/TfheRust/IR/TfheRustOps.h
+++ include/Dialect/TfheRust/IR/TfheRustOps.h
@@ -2,12 +2,10 @@
 #define INCLUDE_DIALECT_TFHERUST_IR_TFHERUSTOPS_H_
 
 #include "include/Dialect/TfheRust/IR/TfheRustDialect.h"
-#include "include/Dialect/TfheRust/IR/TfheRustTraits.h"
 #include "include/Dialect/TfheRust/IR/TfheRustTypes.h"
 #include "mlir/include/mlir/IR/BuiltinOps.h"    // from @llvm-project
 #include "mlir/include/mlir/IR/BuiltinTypes.h"  // from @llvm-project
 #include "mlir/include/mlir/IR/Dialect.h"       // from @llvm-project
-#include "mlir/include/mlir/Interfaces/InferTypeOpInterface.h"  // from @llvm-project
 
 #define GET_OP_CLASSES
 #include "include/Dialect/TfheRust/IR/TfheRustOps.h.inc"
---
+++ lib/Dialect/TfheRust/IR/TfheRustDialect.cpp
@@ -0,0 +1,37 @@
+#include "include/Dialect/TfheRust/IR/TfheRustDialect.h"
+
+#include "include/Dialect/TfheRust/IR/TfheRustDialect.cpp.inc"
+#include "include/Dialect/TfheRust/IR/TfheRustOps.h"
+#include "include/Dialect/TfheRust/IR/TfheRustTypes.h"
+#include "llvm/include/llvm/ADT/TypeSwitch.h"            // from @llvm-project
+#include "mlir/include/mlir/IR/Builders.h"               // from @llvm-project
+#include "mlir/include/mlir/IR/DialectImplementation.h"  // from @llvm-project
+#define GET_TYPEDEF_CLASSES
+#include "include/Dialect/TfheRust/IR/TfheRustTypes.cpp.inc"
+#define GET_OP_CLASSES
+#include "include/Dialect/TfheRust/IR/TfheRustOps.cpp.inc"
+
+namespace mlir {
+namespace heir {
+namespace tfhe_rs {
+
+//===----------------------------------------------------------------------===//
+// TfheRust dialect.
+//===----------------------------------------------------------------------===//
+
+// Dialect construction: there is one instance per context and it registers its
+// operations, types, and interfaces here.
+void TfheRustDialect::initialize() {
+  addTypes<
+#define GET_TYPEDEF_LIST
+#include "include/Dialect/TfheRust/IR/TfheRustTypes.cpp.inc"
+      >();
+  addOperations<
+#define GET_OP_LIST
+#include "include/Dialect/TfheRust/IR/TfheRustOps.cpp.inc"
+      >();
+}
+
+}  // namespace tfhe_rs
+}  // namespace heir
+}  // namespace mlir
