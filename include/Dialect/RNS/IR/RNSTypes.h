e80c25c13262f0519858a37cfae76f6c2fb33ffc
Jeremy Kun <j2kun@users.noreply.github.com>
2024-03-23 11:56:08 -0700

    add initial template generation for RNS dialect

---
+++ include/Dialect/RNS/IR/RNSTypes.h
@@ -0,0 +1,9 @@
+#ifndef INCLUDE_DIALECT_RNS_IR_RNSTYPES_H_
+#define INCLUDE_DIALECT_RNS_IR_RNSTYPES_H_
+
+#include "include/Dialect/RNS/IR/RNSDialect.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "include/Dialect/RNS/IR/RNSTypes.h.inc"
+
+#endif  // INCLUDE_DIALECT_RNS_IR_RNSTYPES_H_
---
+++ lib/Dialect/RNS/IR/RNSTypes.cpp
@@ -0,0 +1,7 @@
+#include "include/Dialect/RNS/IR/RNSTypes.h"
+
+namespace mlir {
+namespace heir {
+namespace rns {}  // namespace rns
+}  // namespace heir
+}  // namespace mlir
