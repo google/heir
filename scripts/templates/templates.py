7a972ca6c487915b380a036512d5a45825c159f4
Jeremy Kun <j2kun@users.noreply.github.com>
2024-05-22 10:43:15 -0700

    migrate template generator to include-less structure

--- scripts/templates/templates.py
+++ scripts/templates/templates.py
@@ -1,6 +1,5 @@
 """A CLI for generating boilerplate MLIR code in HEIR."""
 import os
-import sys
 import pathlib
 import shutil
 
@@ -18,16 +17,13 @@ def render_all(path: pathlib.Path, **args):
       print(f"Rendered template for {path / template_filename}")
 
 
-def try_create_dirs(include_path, lib_path, force=False):
-  print(f"Creating dirs:\n  {include_path}\n  {lib_path}")
+def try_create_dirs(lib_path, force=False):
+  print(f"Creating dirs:\n  {lib_path}")
   try:
-    os.makedirs(include_path)
     os.makedirs(lib_path)
   except FileExistsError:
     if force:
-      shutil.rmtree(include_path)
       shutil.rmtree(lib_path)
-      os.mkdir(include_path)
       os.mkdir(lib_path)
     else:
       raise
@@ -110,44 +106,25 @@ class CLI:
     if not target_dialect_namespace:
       target_dialect_namespace = target_dialect_mnemonic
 
-    include_path = self.root / "include" / "Conversion" / pass_name
     lib_path = self.root / "lib" / "Conversion" / pass_name
 
-    if not force and (os.path.isdir(include_path) or os.path.isdir(lib_path)):
+    if not force and os.path.isdir(lib_path):
       raise ValueError(
-          f"Conversion pass directories already exist at {include_path} or"
-          f" {lib_path}")
+          f"Conversion pass directories already exist at {lib_path}")
 
     templates_path = self.root / "scripts" / "templates" / "Conversion"
-    templ_include = templates_path / "include"
     templ_lib = templates_path / "lib"
     path_mapping = {
-        templ_include / "BUILD.jinja":
-            include_path / "BUILD",
-        templ_include / "ConversionPass.h.jinja":
-            include_path / f"{pass_name}.h",
-        templ_include / "ConversionPass.td.jinja":
-            include_path / f"{pass_name}.td",
-        templ_lib / "BUILD.jinja":
-            lib_path / "BUILD",
-        templ_lib / "ConversionPass.cpp.jinja":
-            lib_path / f"{pass_name}.cpp",
+        templ_lib / "ConversionPass.h.jinja": lib_path / f"{pass_name}.h",
+        templ_lib / "ConversionPass.td.jinja": lib_path / f"{pass_name}.td",
+        templ_lib / "BUILD.jinja": lib_path / "BUILD",
+        templ_lib / "ConversionPass.cpp.jinja": lib_path / f"{pass_name}.cpp",
     }
 
     try:
-      try_create_dirs(include_path, lib_path, force)
+      try_create_dirs(lib_path, force)
       copy_all(path_mapping)
 
-      render_all(
-          include_path,
-          pass_name=pass_name,
-          source_dialect_name=source_dialect_name,
-          source_dialect_namespace=source_dialect_namespace,
-          source_dialect_mnemonic=source_dialect_mnemonic,
-          target_dialect_name=target_dialect_name,
-          target_dialect_namespace=target_dialect_namespace,
-          target_dialect_mnemonic=target_dialect_mnemonic,
-      )
       render_all(
           lib_path,
           pass_name=pass_name,
@@ -160,7 +137,6 @@ class CLI:
       )
     except:
       print("Hit unrecoverable error, cleaning up")
-      shutil.rmtree(include_path)
       shutil.rmtree(lib_path)
       raise
 
@@ -190,42 +166,32 @@ class CLI:
       raise ValueError("dialect_name must be provided")
 
     if not pass_flag:
-      pass_name = f"{dialect_name.lower()}-{pass_name.lower()}"
+      pass_flag = f"{dialect_name.lower()}-{pass_name.lower()}"
 
     # Default could be smarter: look up the name in the actual tablegen for the
     # dialect or quit if it can't be found
     if not dialect_namespace:
       dialect_namespace = dialect_name.lower()
 
-    include_path = self.root / "include" / "Dialect" / dialect_name / "Transforms"
     lib_path = self.root / "lib" / "Dialect" / dialect_name / "Transforms"
 
-    if not force and (os.path.isdir(include_path) or os.path.isdir(lib_path)):
+    if not force and os.path.isdir(lib_path):
       raise ValueError(
-          f"Pass directories already exist at {include_path} or {lib_path}")
+          f"Pass directories already exist at {lib_path}")
 
     templates_path = self.root / "scripts" / "templates" / "DialectTransforms"
-    templ_include = templates_path / "include"
     templ_lib = templates_path / "lib"
     path_mapping = {
-        templ_include / "BUILD.jinja": include_path / "BUILD",
-        templ_include / "Pass.h.jinja": include_path / f"{pass_name}.h",
-        templ_include / "Passes.h.jinja": include_path / "Passes.h",
-        templ_include / "Passes.td.jinja": include_path / "Passes.td",
         templ_lib / "BUILD.jinja": lib_path / "BUILD",
         templ_lib / "Pass.cpp.jinja": lib_path / f"{pass_name}.cpp",
+        templ_lib / "Pass.h.jinja": lib_path / f"{pass_name}.h",
+        templ_lib / "Passes.h.jinja": lib_path / "Passes.h",
+        templ_lib / "Passes.td.jinja": lib_path / "Passes.td",
     }
 
     try:
-      try_create_dirs(include_path, lib_path, force)
+      try_create_dirs(lib_path, force)
       copy_all(path_mapping)
-      render_all(
-          include_path,
-          pass_name=pass_name,
-          pass_flag=pass_flag,
-          dialect_name=dialect_name,
-          dialect_namespace=dialect_namespace,
-      )
       render_all(
           lib_path,
           pass_name=pass_name,
@@ -235,7 +201,6 @@ class CLI:
       )
     except:
       print("Hit unrecoverable error, cleaning up")
-      shutil.rmtree(include_path)
       shutil.rmtree(lib_path)
       raise
 
@@ -259,32 +224,24 @@ class CLI:
     if not pass_flag:
       pass_name = f"{pass_name.lower()}"
 
-    include_path = self.root / "include" / "Transforms" / pass_name
     lib_path = self.root / "lib" / "Transforms" / pass_name
 
-    if not force and (os.path.isdir(include_path) or os.path.isdir(lib_path)):
+    if not force and os.path.isdir(lib_path):
       raise ValueError(
-          f"Pass directories already exist at {include_path} or {lib_path}")
+          f"Pass directories already exist at {lib_path}")
 
     templates_path = self.root / "scripts" / "templates" / "Transforms"
-    templ_include = templates_path / "include"
     templ_lib = templates_path / "lib"
     path_mapping = {
-        templ_include / "BUILD.jinja": include_path / "BUILD",
-        templ_include / "Pass.h.jinja": include_path / f"{pass_name}.h",
-        templ_include / "Pass.td.jinja": include_path / f"{pass_name}.td",
         templ_lib / "BUILD.jinja": lib_path / "BUILD",
         templ_lib / "Pass.cpp.jinja": lib_path / f"{pass_name}.cpp",
+        templ_lib / "Pass.h.jinja": lib_path / f"{pass_name}.h",
+        templ_lib / "Pass.td.jinja": lib_path / f"{pass_name}.td",
     }
 
     try:
-      try_create_dirs(include_path, lib_path, force)
+      try_create_dirs(lib_path, force)
       copy_all(path_mapping)
-      render_all(
-          include_path,
-          pass_name=pass_name,
-          pass_flag=pass_flag,
-      )
       render_all(
           lib_path,
           pass_name=pass_name,
@@ -292,7 +249,6 @@ class CLI:
       )
     except:
       print("Hit unrecoverable error, cleaning up")
-      shutil.rmtree(include_path)
       shutil.rmtree(lib_path)
       raise
 
@@ -325,31 +281,28 @@ class CLI:
     if not dialect_namespace:
       dialect_namespace = dialect_name.lower()
 
-    include_path = self.root / "include" / "Dialect" / dialect_name / "IR"
     lib_path = self.root / "lib" / "Dialect" / dialect_name / "IR"
 
-    if not force and (os.path.isdir(include_path) or os.path.isdir(lib_path)):
+    if not force and os.path.isdir(lib_path):
       raise ValueError(
-          f"Dialect directories already exist at {include_path} or {lib_path}")
+          f"Dialect directories already exist at {lib_path}")
 
     templates_path = self.root / "scripts" / "templates" / "Dialect"
-    templ_include = templates_path / "include"
     templ_lib = templates_path / "lib"
     path_mapping = {
-        templ_include / "BUILD.jinja": include_path / "BUILD",
-        templ_include / "Dialect.h.jinja": include_path / f"{dialect_name}Dialect.h",
-        templ_include / "Dialect.td.jinja": include_path / f"{dialect_name}Dialect.td",
         templ_lib / "BUILD.jinja": lib_path / "BUILD",
         templ_lib / "Dialect.cpp.jinja": lib_path / f"{dialect_name}Dialect.cpp",
+        templ_lib / "Dialect.h.jinja": lib_path / f"{dialect_name}Dialect.h",
+        templ_lib / "Dialect.td.jinja": lib_path / f"{dialect_name}Dialect.td",
     }
 
     if enable_attributes:
       path_mapping.update({
-          templ_include
-          / "Attributes.h.jinja": include_path / f"{dialect_name}Attributes.h",
-          templ_include
+          templ_lib
+          / "Attributes.h.jinja": lib_path / f"{dialect_name}Attributes.h",
+          templ_lib
           / "Attributes.td.jinja": (
-              include_path / f"{dialect_name}Attributes.td"
+              lib_path / f"{dialect_name}Attributes.td"
           ),
           templ_lib
           / "Attributes.cpp.jinja": lib_path / f"{dialect_name}Attributes.cpp",
@@ -357,32 +310,24 @@ class CLI:
 
     if enable_types:
       path_mapping.update({
-          templ_include
-          / "Types.h.jinja": include_path / f"{dialect_name}Types.h",
-          templ_include
-          / "Types.td.jinja": include_path / f"{dialect_name}Types.td",
+          templ_lib
+          / "Types.h.jinja": lib_path / f"{dialect_name}Types.h",
+          templ_lib
+          / "Types.td.jinja": lib_path / f"{dialect_name}Types.td",
           templ_lib / "Types.cpp.jinja": lib_path / f"{dialect_name}Types.cpp",
       })
 
     if enable_ops:
       path_mapping.update({
-          templ_include / "Ops.h.jinja": include_path / f"{dialect_name}Ops.h",
-          templ_include
-          / "Ops.td.jinja": include_path / f"{dialect_name}Ops.td",
+          templ_lib / "Ops.h.jinja": lib_path / f"{dialect_name}Ops.h",
+          templ_lib
+          / "Ops.td.jinja": lib_path / f"{dialect_name}Ops.td",
           templ_lib / "Ops.cpp.jinja": lib_path / f"{dialect_name}Ops.cpp",
       })
 
     try:
-      try_create_dirs(include_path, lib_path, force)
+      try_create_dirs(lib_path, force)
       copy_all(path_mapping)
-      render_all(
-          include_path,
-          dialect_name=dialect_name,
-          dialect_namespace=dialect_namespace,
-          enable_attributes=enable_attributes,
-          enable_types=enable_types,
-          enable_ops=enable_ops,
-      )
       render_all(
           lib_path,
           dialect_name=dialect_name,
@@ -393,14 +338,9 @@ class CLI:
       )
     except:
       print("Hit unrecoverable error, cleaning up")
-      shutil.rmtree(include_path)
       shutil.rmtree(lib_path)
       raise
 
 
 if __name__ == "__main__":
-  # TODO(#659): migrate templates script to include-less world
-  print("Template generator is currently broken, see "
-        "https://github.com/google/heir/issues/659")
-  sys.exit(1)
   fire.Fire(CLI)
