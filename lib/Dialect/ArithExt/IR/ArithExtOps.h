1f41a17b1150af66a7c81a0b3f9168139de14354
Finn Plummer <canadienfinn@gmail.com>
2024-05-31 12:47:43 -0700

    add a new arith_ext dialect
      - Add operations barrett_reduce and subifge to mirror the required
        operations of the HEaan.mlir paper

---
+++ lib/Dialect/ArithExt/IR/ArithExtOps.h
@@ -0,0 +1,11 @@
+#ifndef LIB_DIALECT_ARITHEXT_IR_ARITHEXTOPS_H_
+#define LIB_DIALECT_ARITHEXT_IR_ARITHEXTOPS_H_
+
+#include "lib/Dialect/ArithExt/IR/ArithExtDialect.h"
+#include "mlir/include/mlir/IR/BuiltinOps.h"  // from @llvm-project
+#include "mlir/include/mlir/Interfaces/InferTypeOpInterface.h"  // from @llvm-project
+
+#define GET_OP_CLASSES
+#include "lib/Dialect/ArithExt/IR/ArithExtOps.h.inc"
+
+#endif  // LIB_DIALECT_ARITHEXT_IR_ARITHEXTOPS_H_
