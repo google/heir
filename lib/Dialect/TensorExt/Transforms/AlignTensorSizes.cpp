10e4f0ad841cbc5550a3013a949d48a2b594d5e8
Asra Ali <asraa@google.com>
2024-07-22 14:28:43 -0700

    align-tensor-sizes: add support for tensors larger than desired size
    This PR now supports splitting tensors across multiple `size` shaped tensors to support a basic SIMD packing into ciphertexts with a fixed number of slots.
    
    Part of #704
    
    PiperOrigin-RevId: 654896576

--- lib/Dialect/TensorExt/Transforms/AlignTensorSizes.cpp
+++ lib/Dialect/TensorExt/Transforms/AlignTensorSizes.cpp
@@ -9,18 +9,22 @@
 #include "lib/Dialect/Secret/IR/SecretTypes.h"
 #include "lib/Dialect/TensorExt/IR/TensorExtAttributes.h"
 #include "lib/Dialect/TensorExt/IR/TensorExtDialect.h"
-#include "llvm/include/llvm/ADT/ArrayRef.h"           // from @llvm-project
-#include "llvm/include/llvm/Support/MathExtras.h"     // from @llvm-project
-#include "mlir/include/mlir/IR/Builders.h"            // from @llvm-project
-#include "mlir/include/mlir/IR/BuiltinAttributes.h"   // from @llvm-project
-#include "mlir/include/mlir/IR/BuiltinOps.h"          // from @llvm-project
-#include "mlir/include/mlir/IR/BuiltinTypes.h"        // from @llvm-project
-#include "mlir/include/mlir/IR/Diagnostics.h"         // from @llvm-project
-#include "mlir/include/mlir/IR/OperationSupport.h"    // from @llvm-project
-#include "mlir/include/mlir/IR/PatternMatch.h"        // from @llvm-project
-#include "mlir/include/mlir/IR/Value.h"               // from @llvm-project
-#include "mlir/include/mlir/Support/LLVM.h"           // from @llvm-project
-#include "mlir/include/mlir/Support/LogicalResult.h"  // from @llvm-project
+#include "llvm/include/llvm/ADT/ArrayRef.h"              // from @llvm-project
+#include "llvm/include/llvm/ADT/STLExtras.h"             // from @llvm-project
+#include "llvm/include/llvm/Support/MathExtras.h"        // from @llvm-project
+#include "mlir/include/mlir/Dialect/Arith/IR/Arith.h"    // from @llvm-project
+#include "mlir/include/mlir/Dialect/Tensor/IR/Tensor.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/Builders.h"               // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinAttributes.h"      // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinOps.h"             // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinTypes.h"           // from @llvm-project
+#include "mlir/include/mlir/IR/Diagnostics.h"            // from @llvm-project
+#include "mlir/include/mlir/IR/OperationSupport.h"       // from @llvm-project
+#include "mlir/include/mlir/IR/PatternMatch.h"           // from @llvm-project
+#include "mlir/include/mlir/IR/Value.h"                  // from @llvm-project
+#include "mlir/include/mlir/IR/Visitors.h"               // from @llvm-project
+#include "mlir/include/mlir/Support/LLVM.h"              // from @llvm-project
+#include "mlir/include/mlir/Support/LogicalResult.h"     // from @llvm-project
 #include "mlir/include/mlir/Transforms/DialectConversion.h"  // from @llvm-project
 
 #define DEBUG_NAME "align-tensor-sizes"
@@ -39,21 +43,18 @@ class SecretTensorTypeConverter : public TypeConverter {
 
     addConversion([&](RankedTensorType type) -> std::optional<Type> {
       auto ctx = type.getContext();
-      if (type.getRank() != 1) {
-        emitError(UnknownLoc::get(ctx), "expected 1-D tensor, got") << type;
-        return std::nullopt;
+      if (dyn_cast_or_null<SIMDPackingAttr>(type.getEncoding())) {
+        // This type already has a SIMD packing attribute.
+        return type;
       }
 
       auto dimension = type.getShape()[0];
       if (dimension != size_) {
         SmallVector<int64_t> newShape = {this->size_};
-        // TODO(#704): Handle splitting a tensor across multiple tensors.
         if (dimension > size_) {
-          emitError(UnknownLoc::get(ctx),
-                    "pass only supports tensors with dimension less "
-                    "than or equal to ")
-              << size_ << " , got " << type;
-          return std::nullopt;
+          // Split the tensor into a multi-dimensional tensor.
+          newShape = {static_cast<int64_t>(llvm::divideCeil(dimension, size_)),
+                      size_};
         }
         auto padding = DenseI64ArrayAttr::get(
             ctx,
@@ -80,21 +81,97 @@ class SecretTensorTypeConverter : public TypeConverter {
   int size_;
 };
 
+struct ConvertTensorExtractOp : public OpConversionPattern<tensor::ExtractOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  ConvertTensorExtractOp(MLIRContext *context)
+      : OpConversionPattern<tensor::ExtractOp>(context, 2) {}
+
+  LogicalResult matchAndRewrite(
+      tensor::ExtractOp op, OpAdaptor adaptor,
+      ConversionPatternRewriter &rewriter) const override {
+    auto newTensorType = cast<RankedTensorType>(adaptor.getTensor().getType());
+
+    auto size = newTensorType.getShape()[1];
+    auto index =
+        cast<arith::ConstantIndexOp>(op.getIndices()[0].getDefiningOp())
+            .value();
+    SmallVector<Value> newIndices(
+        {rewriter.create<arith::ConstantIndexOp>(op.getLoc(), index / size),
+         rewriter.create<arith::ConstantIndexOp>(op.getLoc(), index % size)});
+
+    auto newOp = rewriter.create<tensor::ExtractOp>(
+        op->getLoc(), newTensorType.getElementType(), adaptor.getTensor(),
+        newIndices);
+
+    rewriter.replaceOp(op, newOp);
+    return success();
+  }
+};
+
+struct ConvertTensorInsertOp : public OpConversionPattern<tensor::InsertOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  ConvertTensorInsertOp(MLIRContext *context)
+      : OpConversionPattern<tensor::InsertOp>(context, 2) {}
+
+  LogicalResult matchAndRewrite(
+      tensor::InsertOp op, OpAdaptor adaptor,
+      ConversionPatternRewriter &rewriter) const override {
+    auto newTensorType = cast<RankedTensorType>(adaptor.getDest().getType());
+
+    auto size = newTensorType.getShape()[1];
+    auto index =
+        cast<arith::ConstantIndexOp>(op.getIndices()[0].getDefiningOp())
+            .value();
+    SmallVector<Value> newIndices(
+        {rewriter.create<arith::ConstantIndexOp>(op.getLoc(), index / size),
+         rewriter.create<arith::ConstantIndexOp>(op.getLoc(), index % size)});
+
+    auto newOp = rewriter.create<tensor::InsertOp>(
+        op->getLoc(), adaptor.getScalar(), adaptor.getDest(), newIndices);
+
+    rewriter.replaceOp(op, newOp);
+    return success();
+  }
+};
+
 // Promote tensor types to a tensor with fixed final dimension and a
 // SIMDPackingAttr describing the transformation.
 struct AlignTensorSizes : impl::AlignTensorSizesBase<AlignTensorSizes> {
   using AlignTensorSizesBase::AlignTensorSizesBase;
 
   void runOnOperation() override {
-    // Open questions: should this only run on secret tensor types? For
-    // plaintext matrices, we likely need to pack into a ciphertext if it is
-    // cheaper than many MulPlains. (e.g. a dot product with a plaintext weight
-    // matrix).
+    // Pass currently requires that all tensors are 1-D. A smarter pass should
+    // run before this one to customize encoding large dimensional plaintexts
+    // and lowering of higher-level ops.
+    auto result = getOperation()->walk([](Operation *op) {
+      auto multidimensional =
+          llvm::any_of(op->getOperandTypes(), [](Type type) {
+            if (auto tensorType = dyn_cast<RankedTensorType>(type)) {
+              return tensorType.getRank() != 1;
+            }
+            return false;
+          });
+      multidimensional |= llvm::any_of(op->getResultTypes(), [](Type type) {
+        if (auto tensorType = dyn_cast<RankedTensorType>(type)) {
+          return tensorType.getRank() != 1;
+        }
+        return false;
+      });
+      return multidimensional ? WalkResult::interrupt() : WalkResult::advance();
+    });
+    if (result.wasInterrupted()) {
+      signalPassFailure();
+      return;
+    }
+
     MLIRContext *context = &getContext();
     SecretTensorTypeConverter typeConverter(size);
 
     RewritePatternSet patterns(context);
-    patterns.add<ConvertAny>(typeConverter, context);
+    patterns.add<ConvertAny, ConvertTensorExtractOp, ConvertTensorInsertOp>(
+        typeConverter, context);
 
     ConversionTarget target(*context);
     target.addLegalOp<ModuleOp>();
@@ -104,6 +181,14 @@ struct AlignTensorSizes : impl::AlignTensorSizesBase<AlignTensorSizes> {
     // here. This is a bit hacky.
     target.markUnknownOpDynamicallyLegal(
         [&](Operation *op) { return typeConverter.isLegal(op); });
+    target.addDynamicallyLegalOp<tensor::ExtractOp>([&](tensor::ExtractOp op) {
+      return typeConverter.isLegal(op) &&
+             op.getIndices().size() == op.getTensor().getType().getRank();
+    });
+    target.addDynamicallyLegalOp<tensor::InsertOp>([&](tensor::InsertOp op) {
+      return typeConverter.isLegal(op) &&
+             op.getIndices().size() == op.getDest().getType().getRank();
+    });
 
     if (failed(applyPartialConversion(getOperation(), target,
                                       std::move(patterns)))) {
--- tests/tensor_ext/simd_pack.mlir
+++ tests/tensor_ext/simd_pack.mlir
@@ -1,15 +1,39 @@
-// RUN: heir-opt --secretize=entry-function=add --wrap-generic --canonicalize --cse \
-// RUN:   --align-tensor-sizes %s | FileCheck %s
+// RUN: heir-opt --secretize=entry-function=main --wrap-generic \
+// RUN:   --align-tensor-sizes --canonicalize --cse --split-input-file %s | FileCheck %s
 
 module  {
-  // CHECK-LABEL: @add
+  // CHECK-LABEL: @main
   // CHECK-SAME{LITERAL}: !secret.secret<tensor<1024xi16, #tensor_ext.simd_packing<in = [16], padding = [0], out = [1024]>>>
-  func.func @add(%arg0: tensor<16xi16>, %arg1: tensor<16xi16>) -> tensor<16xi16> {
+  func.func @main(%arg0: tensor<16xi16>, %arg1: tensor<16xi16>) -> tensor<16xi16> {
     // CHECK-NEXT: secret.generic ins(%[[arg0:.*]], %[[arg1:.*]] : !secret.secret<[[ty:.*]]>, !secret.secret<[[ty]]>)
     // CHECK-NEXT:  ^bb0(%[[arg0_0:.*]]: [[ty]], %[[arg1_0:.*]]: [[ty]]):
     // CHECK-NEXT:    %[[add:.*]] = arith.addi %[[arg0_0]], %[[arg1_0]] : [[ty]]
     // CHECK-NEXT:    secret.yield %[[add]] : [[ty]]
     %0 = arith.addi %arg0, %arg1 : tensor<16xi16>
     return %0 : tensor<16xi16>
+  }
+}
+
+// -----
+
+module  {
+  // CHECK-LABEL: @main
+  // CHECK-SAME: %[[arg0:.*]]:
+  // CHECK-SAME{LITERAL}: tensor<2x1024xi16, #tensor_ext.simd_packing<in = [2048], padding = [0], out = [2, 1024]>>
+  func.func @main(%arg0: tensor<2048xi16>) -> tensor<2048xi16> {
+    // CHECK-DAG: %[[c1:.*]] = arith.constant 1 : index
+    // CHECK-DAG: %[[c976:.*]] = arith.constant 976 : index
+    // CHECK-DAG: %[[c0:.*]] = arith.constant 0 : index
+    // CHECK-DAG: %[[c20:.*]] = arith.constant 20 : index
+    // CHECK: secret.generic ins(%[[arg0]] : !secret.secret<[[ty:.*]]>)
+    // CHECK-NEXT:  ^bb0(%[[arg0_0:.*]]: [[ty]]):
+    // CHECK-NEXT:    %[[extract1:.*]] = tensor.extract %[[arg0_0]][%[[c1]], %[[c976]]] : [[ty]]
+    // CHECK-NEXT:    %[[extracted:.*]] = tensor.insert %[[extract1]] into %[[arg0_0]][%[[c0]], %[[c20]]] : [[ty]]
+    // CHECK-NEXT:    secret.yield %[[extracted]]
+    %c20 = arith.constant 20 : index
+    %c2000 = arith.constant 2000 : index
+    %1 = tensor.extract %arg0[%c2000] : tensor<2048xi16>
+    %inserted = tensor.insert %1 into %arg0[%c20] : tensor<2048xi16>
+    return %inserted : tensor<2048xi16>
   }
 }
