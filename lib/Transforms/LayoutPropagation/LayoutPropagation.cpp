aa4dc15db7c821ad0e67bf50a1c8f5fedfb89276
Jeremy Kun <j2kun@users.noreply.github.com>
2025-01-17 17:15:16 -0800

    support layout propagation for all ops in halevi-shoup e2e example

--- lib/Transforms/LayoutPropagation/LayoutPropagation.cpp
+++ lib/Transforms/LayoutPropagation/LayoutPropagation.cpp
@@ -27,6 +27,7 @@ using secret::SecretType;
 using secret::YieldOp;
 using tensor::CollapseShapeOp;
 using tensor::EmptyOp;
+using tensor::ExpandShapeOp;
 using tensor_ext::ConvertLayoutOp;
 using tensor_ext::LayoutAttr;
 using tensor_ext::SumOp;
@@ -44,6 +45,7 @@ struct LayoutPropagation : impl::LayoutPropagationBase<LayoutPropagation> {
   // Op-specific transfer functions
   LogicalResult visitOperation(AddIOp op);
   LogicalResult visitOperation(CollapseShapeOp op);
+  LogicalResult visitOperation(ExpandShapeOp op);
   LogicalResult visitOperation(EmptyOp op);
   LogicalResult visitOperation(GenericOp op);
   LogicalResult visitOperation(MulIOp op);
@@ -51,6 +53,15 @@ struct LayoutPropagation : impl::LayoutPropagationBase<LayoutPropagation> {
   LogicalResult visitOperation(VecmatOp op);
   LogicalResult visitOperation(YieldOp op);
   LogicalResult visitOperation(func::FuncOp op);
+  LogicalResult visitOperation(func::ReturnOp op);
+
+  // Return true if the operand layouts are compatible for the operation, and
+  // false if not. Include an InFlightDiagnostic if an operand is encountered
+  // that requires a layout, but none has been set.
+  std::pair<bool, std::optional<InFlightDiagnostic>>
+  hasCompatibleArgumentLayouts(Operation *op);
+  // Insert conversion ops to rectify incompatible operand layouts
+  void rectifyIncompatibleOperandLayouts(Operation *op);
 
   // Return the default layout for a given type
   FailureOr<AffineMap> defaultLayoutForType(Type type);
@@ -77,91 +88,48 @@ void debugAssignLayout(Value value, AffineMap layout) {
                           << value << "\n");
 }
 
-// A helper struct to keep track of an operand's index and layout.
-struct OperandLayout {
-  Value operand;
-  unsigned int index;
-  AffineMap layout;
-};
-
 LogicalResult LayoutPropagation::visitOperation(Operation *op) {
   visitDebugInfo(op);
 
-  if (!isSecret(op->getOperands(), solver) &&
+  // Note, generic ops with no operands can still produce secret results,
+  // (e.g., tensor.empty which is then yielded, see below) but the secretness
+  // analysis doesn't detect this as a secret result.
+  //
+  //  %0 = secret.generic {
+  //    %2 = tensor.empty() : tensor<16x16xf32>
+  //    secret.yield %2 : tensor<16x16xf32>
+  //  } -> !secret.secret<tensor<16x16xf32>>
+  //
+  // Moreover, we handle the generic's result by processing the terminator
+  // yield op, so even if the secretness analysis worked on the secret.generic,
+  // we'd still need it to work on the yield, and in this case it doesn't since
+  // there's no upstream dependency on a secret operand. Manage this by skipping
+  // the check on YieldOp here and having a special case in the handling of
+  // YieldOp that allows one to yield a value without any existing layout set.
+  //
+  // FIXME: should I expand the secretness analysis to handle secret.yield and
+  // potentially secret.generic specially?
+  if (!isa<func::FuncOp, func::ReturnOp, GenericOp, YieldOp>(op) &&
+      !isSecret(op->getOperands(), solver) &&
       !isSecret(op->getResults(), solver)) {
     LLVM_DEBUG(llvm::dbgs() << "Skipping op " << op->getName()
                             << " with no secret operands or results\n");
     return success();
   }
 
-  mlir::IRRewriter builder(&getContext());
-  // When the operands have different layout attributes, it is invalid
-  // to apply the op, and we must insert layout conversion ops.
-  SmallVector<OperandLayout> layouts;
-  bool disagreeingLayouts = false;
-  std::optional<AffineMap> targetLayout;
-
-  for (auto &operand : op->getOpOperands()) {
-    if (isa<RankedTensorType>(operand.get().getType())) {
-      // FIXME: ciphertext-plaintext ops should be supported here
-      if (assignedLayouts.count(operand.get()) == 0) {
-        // If the operand has no layout, we can't propagate layout
-        // information to the result.
-        return op->emitError("operand has no assigned layout");
-      }
-      AffineMap layout = assignedLayouts[operand.get()];
-      OperandLayout operandLayout = {operand.get(), operand.getOperandNumber(),
-                                     layout};
-      layouts.push_back(operandLayout);
-
-      // Here we choose an arbitrary target layout. Another pass is expected to
-      // optimize for the choice of conversions.
-      if (!targetLayout.has_value()) targetLayout = layout;
-
-      if (layout != targetLayout.value()) {
-        disagreeingLayouts = true;
-      }
+  auto [compatible, diag] = hasCompatibleArgumentLayouts(op);
+  if (!compatible) {
+    if (diag.has_value()) {
+      // An InFlightDiagnostic casts to a failure()
+      return diag.value();
     }
-  }
-
-  // If the operands have different layouts, we need to insert layout
-  // conversion ops.
-  if (!layouts.empty() && disagreeingLayouts) {
-    LLVM_DEBUG({
-      auto diag = op->emitRemark() << "Inserting layout conversion op due to "
-                                      "disagreeing operand layouts";
-      auto &note = diag.attachNote();
-      for (auto operandLayout : layouts) {
-        std::string mapStr;
-        llvm::raw_string_ostream os(mapStr);
-        operandLayout.layout.print(os);
-        note << "\n- Operand: " << operandLayout.operand
-             << "; Layout: " << os.str() << "\n";
-      }
-    });
-
-    for (auto &operandLayout : layouts) {
-      Value operand = operandLayout.operand;
-      AffineMap sourceLayout = operandLayout.layout;
-      if (sourceLayout != targetLayout) {
-        builder.setInsertionPoint(op);
-        ConvertLayoutOp convertOp = builder.create<ConvertLayoutOp>(
-            op->getLoc(), operand, AffineMapAttr::get(sourceLayout),
-            AffineMapAttr::get(targetLayout.value()));
-
-        // Layout of the result is the same as the target layout of the
-        // conversion. Mostly this is done for consistency: all ops have an
-        // attribute describing the layout of their results.
-        OpBuilder builder(&getContext());
-        assignedLayouts.insert({convertOp.getResult(), targetLayout.value()});
-        setResultLayoutAttr(convertOp);
-        op->setOperand(operandLayout.index, convertOp.getResult());
-      }
-    }
+    rectifyIncompatibleOperandLayouts(op);
   }
 
   return TypeSwitch<Operation *, LogicalResult>(op)
-      .Case<func::FuncOp>([&](auto op) { return visitOperation(op); })
+      // func ops
+      .Case<func::FuncOp, func::ReturnOp>(
+          [&](auto op) { return visitOperation(op); })
       // arith ops
       .Case<AddIOp, MulIOp>([&](auto op) { return visitOperation(op); })
       // secret ops
@@ -171,9 +139,102 @@ LogicalResult LayoutPropagation::visitOperation(Operation *op) {
       // linalg ops
       .Case<VecmatOp>([&](auto op) { return visitOperation(op); })
       // tensor ops
-      .Case<CollapseShapeOp, EmptyOp>(
+      .Case<CollapseShapeOp, ExpandShapeOp, EmptyOp>(
           [&](auto op) { return visitOperation(op); })
       .Default([&](Operation *op) { return success(); });
+}
+
+std::pair<bool, std::optional<InFlightDiagnostic>>
+LayoutPropagation::hasCompatibleArgumentLayouts(Operation *op) {
+  if (isa<func::FuncOp, GenericOp, YieldOp>(op)) {
+    return {true, std::nullopt};
+  }
+
+  if (isa<VecmatOp>(op)) {
+    // Currently only support secret vectors and plaintext matrices.
+    auto vecmatOp = cast<linalg::ContractionOpInterface>(op);
+    Value vec = vecmatOp.lhs();
+    Value mat = vecmatOp.rhs();
+    if (isSecret(mat, solver) || !isSecret(vec, solver)) {
+      return {false,
+              op->emitError("Only secret vectors and plaintext matrices are "
+                            "supported for linalg.vecmat")};
+    }
+
+    if (!assignedLayouts.contains(vec)) {
+      return {false, op->emitError("vector operand has no assigned layout")};
+    }
+    return {true, std::nullopt};
+  }
+
+  // By default, assume operands must all have the same layout.
+  std::optional<AffineMap> firstFoundLayout;
+
+  for (auto &operand : op->getOpOperands()) {
+    if (isa<RankedTensorType>(operand.get().getType())) {
+      if (!assignedLayouts.contains(operand.get())) {
+        // If the operand has no layout, we can't propagate layout
+        // information to the result.
+        return {false, op->emitError("operand has no assigned layout")};
+      }
+      AffineMap layout = assignedLayouts.at(operand.get());
+
+      if (!firstFoundLayout.has_value()) firstFoundLayout = layout;
+      if (layout != firstFoundLayout.value()) {
+        return {false, std::nullopt};
+      }
+    }
+  }
+
+  return {true, std::nullopt};
+}
+
+void LayoutPropagation::rectifyIncompatibleOperandLayouts(Operation *op) {
+  mlir::IRRewriter builder(&getContext());
+
+  LLVM_DEBUG({
+    auto diag = op->emitRemark() << "Inserting layout conversion op due to "
+                                    "disagreeing operand layouts";
+    auto &note = diag.attachNote();
+    for (auto operand : op->getOperands()) {
+      std::string mapStr;
+      llvm::raw_string_ostream os(mapStr);
+      AffineMap operandLayout;
+      if (assignedLayouts.contains(operand))
+        operandLayout = assignedLayouts.at(operand);
+      operandLayout.print(os);
+      note << "\n- Operand: " << operand << "; Layout: " << os.str() << "\n";
+    }
+  });
+
+  // Target layout is chosen arbitrarily for now. A different pass is
+  // responsible for optimizing the placement and mechanics of the layout
+  // conversion ops. The hasCompatibleArgumentLayouts method's failure ensures
+  // a layout is present for at least two operands, and they are incompatible.
+  const auto it = llvm::find_if(op->getOperands(), [this](Value pair) {
+    return assignedLayouts.contains(pair);
+  });
+  AffineMap targetLayout = assignedLayouts.at(*it);
+
+  for (auto &opOperand : op->getOpOperands()) {
+    if (!assignedLayouts.contains(opOperand.get())) continue;
+    AffineMap sourceLayout = assignedLayouts.at(opOperand.get());
+
+    if (sourceLayout != targetLayout) {
+      builder.setInsertionPoint(op);
+      ConvertLayoutOp convertOp = builder.create<ConvertLayoutOp>(
+          op->getLoc(), opOperand.get(), AffineMapAttr::get(sourceLayout),
+          AffineMapAttr::get(targetLayout));
+
+      // Layout of the result is the same as the target layout of the
+      // conversion. Mostly this is done for consistency: all ops have an
+      // attribute describing the layout of their results.
+      OpBuilder builder(&getContext());
+      assignedLayouts.insert({convertOp.getResult(), targetLayout});
+      setResultLayoutAttr(convertOp);
+      op->setOperand(opOperand.getOperandNumber(), convertOp.getResult());
+    }
+  }
 }
 
 LogicalResult LayoutPropagation::visitOperation(func::FuncOp op) {
@@ -194,23 +255,43 @@ LogicalResult LayoutPropagation::visitOperation(func::FuncOp op) {
     ++argIndex;
   }
 
+  // Func result attrs are handled by the ReturnOp
+  return success();
+}
+
+LogicalResult LayoutPropagation::visitOperation(func::ReturnOp op) {
+  func::FuncOp func = op->getParentOfType<func::FuncOp>();
+  for (OpOperand &operand : op->getOpOperands()) {
+    if (!assignedLayouts.contains(operand.get())) {
+      if (isSecret(operand.get(), solver)) {
+        return op->emitError("secret return value has no assigned layout");
+      }
+
+      // It needs no layout.
+      continue;
+    }
+    AffineMap layout = assignedLayouts.at(operand.get());
+    func.setResultAttr(operand.getOperandNumber(),
+                       tensor_ext::TensorExtDialect::kLayoutAttrName,
+                       LayoutAttr::get(&getContext(), layout));
+  }
   return success();
 }
 
 LogicalResult LayoutPropagation::visitOperation(GenericOp op) {
   // Every block argument has the same layout as its corresponding operand.
   for (OpOperand &operand : op->getOpOperands()) {
-    if (assignedLayouts.count(operand.get()) == 0) {
+    if (!assignedLayouts.contains(operand.get())) {
       // Assume it is not a tensor type and doesn't need a layout.
       continue;
     }
-    AffineMap layout = assignedLayouts[operand.get()];
+    AffineMap layout = assignedLayouts.at(operand.get());
     BlockArgument blockArg =
         op.getRegion().getArgument(operand.getOperandNumber());
     assignedLayouts.insert({blockArg, layout});
     op.setArgAttr(operand.getOperandNumber(), "layout",
                   AffineMapAttr::get(layout));
-    debugAssignLayout(operand.get(), layout);
+    debugAssignLayout(blockArg, layout);
   }
 
   // The layout of the result of the generic op is handled when the YieldOp is
@@ -222,11 +303,27 @@ LogicalResult LayoutPropagation::visitOperation(YieldOp op) {
   // The results of the generic op has the same layouts as the yielded values
   GenericOp generic = op->getParentOfType<GenericOp>();
   for (OpOperand &operand : op->getOpOperands()) {
-    if (assignedLayouts.count(operand.get()) == 0) {
-      // Assume it is not a tensor type and doesn't need a layout.
-      continue;
+    Type operandType = operand.get().getType();
+    if (!assignedLayouts.contains(operand.get())) {
+      // If it's a tensor type, it may be something like a tensor.empty()
+      // that would not be assigned a layout earlier in the walk, because
+      // it does not depend on any secret information. In this case, use the
+      // default layout.
+      LLVM_DEBUG(llvm::dbgs() << "No layout assigned to operand "
+                              << operand.get() << ", using default layout\n");
+      if (isa<RankedTensorType>(operandType)) {
+        FailureOr<AffineMap> layout = defaultLayoutForType(operandType);
+        if (failed(layout)) {
+          return failure();
+        }
+        debugAssignLayout(operand.get(), layout.value());
+        assignedLayouts.insert({operand.get(), layout.value()});
+      } else {
+        // Assume it is not a tensor type and doesn't need a layout.
+        continue;
+      }
     }
-    AffineMap layout = assignedLayouts[operand.get()];
+    AffineMap layout = assignedLayouts.at(operand.get());
     Value result = generic.getResult(operand.getOperandNumber());
     assignedLayouts.insert({result, layout});
     debugAssignLayout(result, layout);
@@ -239,7 +336,7 @@ void LayoutPropagation::passLayoutThroughOp(Operation *op) {
   // All inputs have the same layout, so just propagate it to all results
   for (Value result : op->getResults()) {
     if (isa<RankedTensorType>(result.getType())) {
-      AffineMap layout = assignedLayouts[op->getOperand(0)];
+      AffineMap layout = assignedLayouts.at(op->getOperand(0));
       assignedLayouts.insert({result, layout});
       debugAssignLayout(result, layout);
     }
@@ -271,7 +368,7 @@ LogicalResult LayoutPropagation::visitOperation(CollapseShapeOp op) {
         "Only rank-reduced types are supported for CollapseShapeOp");
 
   auto tensor = op.getSrc();
-  AffineMap inputLayout = assignedLayouts[tensor];
+  AffineMap inputLayout = assignedLayouts.at(tensor);
   unsigned numDims = tensor.getType().getRank();
   llvm::SmallBitVector dimsBV(numDims, false);
 
@@ -295,24 +392,73 @@ LogicalResult LayoutPropagation::visitOperation(CollapseShapeOp op) {
   return success();
 }
 
-LogicalResult LayoutPropagation::visitOperation(VecmatOp op) {
-  auto vecmatOp = cast<linalg::ContractionOpInterface>(op);
-  auto vec = vecmatOp.lhs();
-  auto mat = vecmatOp.rhs();
-  // always one result
-  auto result = vecmatOp->getResult(0);
-
-  // Currently only support secret vectors and plaintext matrices.
-  if (isSecret(mat) || !isSecret(vec)) {
+LogicalResult LayoutPropagation::visitOperation(ExpandShapeOp op) {
+  MLIRContext *context = &getContext();
+  // Only support rank-reduced types for now, i.e., where the expanded shape
+  // only adds static dimensions of size 1.
+  SliceVerificationResult res =
+      isRankReducedType(op.getResultType(), op.getSrcType());
+  if (res != SliceVerificationResult::Success)
     return op->emitError(
-        "Only secret vectors and plaintext matrices are "
-        "supported for linalg.vecmat");
+        "Only rank-reduced types are supported for ExpandShapeOp");
+
+  auto tensor = op.getSrc();
+  AffineMap inputLayout = assignedLayouts.at(tensor);
+
+  // tensor indices correspond to layout dimensions, and adding a dimension of
+  // size 1 has no effect on the affine map expressions, so all we're doing is
+  // adding new dimensions for each reassociation group index corresponding to
+  // an output dimension of size 1. Mainly we have to ensure that the dimension
+  // we're adding is in the correct index of the affine map's dimension list.
+  int oldDim = 0;
+  DenseMap<AffineExpr, AffineExpr> oldDimsToNewDims;
+  for (Attribute associationGroup : op.getReassociation()) {
+    auto associationArray = dyn_cast<ArrayAttr>(associationGroup).getValue();
+    // a single-entry association group is a no-op
+    if (associationArray.size() == 1) {
+      oldDimsToNewDims[getAffineDimExpr(oldDim, context)] = getAffineDimExpr(
+          cast<IntegerAttr>(associationArray[0]).getInt(), context);
+      ++oldDim;
+      continue;
+    }
+
+    for (Attribute association : associationArray) {
+      int64_t reassocDim = cast<IntegerAttr>(association).getInt();
+      if (op.getResultType().getShape()[reassocDim] > 1) {
+        oldDimsToNewDims[getAffineDimExpr(oldDim, context)] =
+            getAffineDimExpr(reassocDim, context);
+        ++oldDim;
+      }
+    }
   }
 
-  AffineMap vecLayout = assignedLayouts[vec];
-  // The matrix has no assigned layout because it is not secret.
+  int resultNumDims = op.getResultType().getRank();
+  // First create a larger-rank affine map, but using old dimension identifiers
+  AffineMap resLayout1 =
+      AffineMap::get(resultNumDims, /*symbolCount=*/0, inputLayout.getResults(),
+                     &getContext());
 
-  // The output layout is the same as the input vector layout.
+  // Then replace the old dimension identifier expressions with new ones
+  AffineMap resultLayout = resLayout1.replace(oldDimsToNewDims);
+
+  assignedLayouts.insert({op.getResult(), resultLayout});
+  setResultLayoutAttr(op);
+  debugAssignLayout(op.getResult(), resultLayout);
+  return success();
+}
+
+LogicalResult LayoutPropagation::visitOperation(VecmatOp op) {
+  auto vecmatOp = cast<linalg::ContractionOpInterface>(*op);
+  auto vec = vecmatOp.lhs();
+
+  // The matrix has no assigned layout because it is assumed to be
+  // plaintext/static (this is intended to be enforced by
+  // hasCompatibleArgumentLayouts).
+  AffineMap vecLayout = assignedLayouts.at(vec);
+
+  // Always one result, and it's a vector with the same layout
+  // as the input vector
+  auto result = vecmatOp->getResult(0);
   AffineMap resultLayout = vecLayout;
 
   assignedLayouts.insert({result, resultLayout});
@@ -336,7 +482,7 @@ LogicalResult LayoutPropagation::visitOperation(SumOp op) {
   Value tensor = op.getTensor();
   Value result = op.getOutput();
 
-  AffineMap inputLayout = assignedLayouts[tensor];
+  AffineMap inputLayout = assignedLayouts.at(tensor);
   // The result layout is equivalent to reducing the summed dimension
   // to 1 and then dropping it.
 
@@ -392,7 +538,8 @@ FailureOr<AffineMap> LayoutPropagation::defaultLayoutForType(Type type) {
 void LayoutPropagation::setResultLayoutAttr(Operation *op) {
   OpBuilder builder(&getContext());
   SmallVector<AffineMap> resultLayouts = llvm::map_to_vector(
-      op->getResults(), [&](Value result) { return assignedLayouts[result]; });
+      op->getResults(),
+      [&](Value result) { return assignedLayouts.at(result); });
   op->setAttr("layout", builder.getAffineMapArrayAttr(resultLayouts));
 }
 
