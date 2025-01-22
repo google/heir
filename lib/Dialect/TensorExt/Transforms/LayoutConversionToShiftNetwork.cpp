79b71f079ccd06542b83629983db14fd4de3ec6a
Jeremy Kun <j2kun@users.noreply.github.com>
2025-01-21 22:51:38 -0800

    keep working on shift network impl

--- lib/Dialect/TensorExt/Transforms/LayoutConversionToShiftNetwork.cpp
+++ lib/Dialect/TensorExt/Transforms/LayoutConversionToShiftNetwork.cpp
@@ -1,6 +1,9 @@
 #include "lib/Dialect/TensorExt/Transforms/LayoutConversionToShiftNetwork.h"
 
+#include <utility>
+
 #include "lib/Dialect/TensorExt/IR/TensorExtOps.h"
+#include "lib/Utils/Graph/Graph.h"
 #include "llvm/include/llvm/ADT/TypeSwitch.h"            // from @llvm-project
 #include "llvm/include/llvm/Support/Debug.h"             // from @llvm-project
 #include "mlir/include/mlir/Dialect/Arith/IR/Arith.h"    // from @llvm-project
@@ -23,18 +26,18 @@ namespace tensor_ext {
 
 // A permutation of 0..n-1. This array ref should always have size n
 // and contain each integer from 0 to n-1 exactly once.
-using Permutation = llvm::OwningArrayRef<int64_t>;
+// FIXME: replace this with a hashable type that can be used with DenseMap
+using Permutation = SmallVector<int64_t>;
 
 // A group of indices to rotate together
 using RotationGroup = DenseSet<int64_t>;
 
-Permutation identity(int64_t n) {
+inline Permutation identity(int64_t n) {
   SmallVector<int64_t> permutation;
   for (int64_t i = 0; i < n; i++) {
     permutation.push_back(i);
   }
-  // Copies the data into the owning array ref
-  return llvm::OwningArrayRef<int64_t>(permutation);
+  return permutation;
 }
 
 // Cf. https://www.jeremykun.com/2024/09/02/shift-networks/
@@ -46,12 +49,15 @@ class VosVosErkinShiftNetworks {
       : ciphertextSize(ciphertextSize) {}
 
   // Computes the shift network for a given permutation of ciphertext indices.
-  FailureOr<ArrayRef<RotationGroup>> computeShiftNetwork(
-      Permutation permutation) {
+  // The returned ArrayRef is owned by this VosVosErkinShiftNetworks object.
+  // The resulting shift is cached, and the cache is used on further calls to
+  // avoid recomputing the shift network.
+  ArrayRef<RotationGroup> computeShiftNetwork(Permutation permutation) {
     if (rotationGroups.count(permutation)) {
-      return FailureOr(rotationGroups[permutation]);
+      return rotationGroups[permutation];
     }
 
+    Permutation identityPerm = identity(ciphertextSize);
     // Stores the amount that each ciphertext index is shifted forward.
     SmallVector<int64_t> shifts;
     for (int64_t i = 0; i < ciphertextSize; i++) {
@@ -66,9 +72,8 @@ class VosVosErkinShiftNetworks {
     for (int64_t rotationAmount = 1; rotationAmount <= ciphertextSize;
          rotationAmount <<= 1) {
       SmallVector<int64_t> round;
-      ArrayRef<int64_t> lastRound = !rounds.empty()
-                                        ? ArrayRef<int64_t>(rounds.back())
-                                        : identity(ciphertextSize);
+      ArrayRef<int64_t> lastRound =
+          !rounds.empty() ? ArrayRef<int64_t>(rounds.back()) : identityPerm;
       int inputIndex = 0;
       for (int64_t shift : shifts) {
         // The bit is set, implying we would rotate by 2**bit in this round
@@ -83,22 +88,51 @@ class VosVosErkinShiftNetworks {
       rounds.push_back(round);
     }
 
-    // FIXME: construct the graph of conflicts, greedy color, set+return
-    // rotation groups
+    // Create a graph whose vertices are the input indices to permute, and
+    // whose edges are conflicts: an edge being present means the two indices
+    // cannot participate in the same rotation group.
+    graph::UndirectedGraph<int64_t> conflictGraph;
+    for (int64_t i = 0; i < ciphertextSize; i++) {
+      conflictGraph.addVertex(i);
+    }
+    for (const SmallVector<int64_t> &round : rounds) {
+      for (int64_t i = 0; i < ciphertextSize; i++) {
+        for (int64_t j = i + 1; j < ciphertextSize; j++) {
+          if (round[i] == round[j]) {
+            conflictGraph.addEdge(i, j);
+          }
+        }
+      }
+    }
 
-    return failure();
+    graph::GreedyGraphColoring<int64_t> colorer;
+    std::unordered_map<int64_t, int> coloring = colorer.color(conflictGraph);
+
+    SmallVector<RotationGroup> resultRotationGroups;
+    rotationGroups.reserve(10);
+    for (const auto &entry : coloring) {
+      int64_t index = entry.first;
+      int64_t color = entry.second;
+      if (color >= rotationGroups.size()) {
+        resultRotationGroups.resize(color + 1);
+      }
+      resultRotationGroups[color].insert(index);
+    }
+
+    rotationGroups[permutation] = resultRotationGroups;
+    return rotationGroups[permutation];
   }
 
  private:
   int64_t ciphertextSize;
-  DenseMap<Permutation, SmallVector<RotationGroup>> rotationGroups;
+  DenseMap<Permutation, llvm::SmallVector<RotationGroup>> rotationGroups;
 };
 
 struct RewriteLayoutConversion : public OpRewritePattern<ConvertLayoutOp> {
   RewriteLayoutConversion(mlir::MLIRContext *context,
                           VosVosErkinShiftNetworks shiftNetworks)
       : mlir::OpRewritePattern<ConvertLayoutOp>(context),
-        shiftNetworks(shiftNetworks) {}
+        shiftNetworks(std::move(shiftNetworks)) {}
 
   LogicalResult matchAndRewrite(ConvertLayoutOp op,
                                 PatternRewriter &rewriter) const override {
@@ -117,7 +151,7 @@ struct LayoutConversionToShiftNetwork
     MLIRContext *context = &getContext();
     RewritePatternSet patterns(context);
 
-    VosVosErkinShiftNetworks shiftNetworks(ciphertextSize);
+    VosVosErkinShiftNetworks shiftNetworks{ciphertextSize};
     patterns.add<RewriteLayoutConversion>(context, shiftNetworks);
 
     (void)walkAndApplyPatterns(getOperation(), std::move(patterns));
