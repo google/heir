ab5a59cac12ceaaaba51ce8310d8673e0fc4234c
Zenithal <i@zenithal.me>
2025-02-07 15:03:36 +0000

    Lattigo: Add debug func impl for noise

--- tests/Examples/lattigo/dot_product_8_debug.go
+++ tests/Examples/lattigo/dot_product_8_debug.go
@@ -13,4 +13,26 @@ func __heir_debug(evaluator *bgv.Evaluator, param bgv.Parameters, encoder *bgv.E
 	pt := decryptor.DecryptNew(ct)
 	encoder.Decode(pt, value)
 	fmt.Println(value)
+
+	// get a new pt with no noise
+	// in Lattigo, Decrypt won't mod T
+	// Decode will mod T so we Decode then Encode
+	valueFull := make([]int64, ct.N())
+	encoder.Decode(pt, valueFull)
+	// set the level and scale of the plaintext
+	ptNoNoise := bgv.NewPlaintext(param, ct.Level())
+	ptNoNoise.Scale = ct.Scale
+	encoder.Encode(valueFull, ptNoNoise)
+
+	// subtract the message from the ciphertext
+	vec, _ := evaluator.SubNew(ct, ptNoNoise)
+	// get infty norm of the noise
+	_, _, max := rlwe.Norm(vec, decryptor)
+	// get logQi for current level
+	total := 0
+	for i := 0; i <= ct.LevelQ(); i++ {
+		total += param.LogQi()[i]
+	}
+	// t * e for BGV
+	fmt.Printf("Noise: %.2f Total: %d\n", max+param.LogT(), total)
 }
