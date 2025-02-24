9fc1ee6b4927cc1e05c7a5f3f403ad4e86a4e5ea
Zenithal <i@zenithal.me>
2025-02-24 14:07:02 +0000

    validate scale

--- lib/Transforms/PopulateScale/PopulateScale.cpp
+++ lib/Transforms/PopulateScale/PopulateScale.cpp
@@ -1,16 +1,20 @@
 #include "lib/Transforms/PopulateScale/PopulateScale.h"
 
+#include "lib/Analysis/ScaleAnalysis/ScaleAnalysis.h"
 #include "lib/Dialect/BGV/IR/BGVAttributes.h"
 #include "lib/Dialect/BGV/IR/BGVDialect.h"
 #include "lib/Dialect/Mgmt/IR/MgmtAttributes.h"
 #include "lib/Dialect/Mgmt/IR/MgmtOps.h"
-#include "llvm/include/llvm/Support/Debug.h"            // from @llvm-project
-#include "mlir/include/mlir/Dialect/Func/IR/FuncOps.h"  // from @llvm-project
-#include "mlir/include/mlir/IR/Operation.h"             // from @llvm-project
-#include "mlir/include/mlir/IR/Value.h"                 // from @llvm-project
-#include "mlir/include/mlir/IR/Visitors.h"              // from @llvm-project
-#include "mlir/include/mlir/Support/LLVM.h"             // from @llvm-project
-#include "mlir/include/mlir/Transforms/Passes.h"        // from @llvm-project
+#include "llvm/include/llvm/Support/Debug.h"  // from @llvm-project
+#include "mlir/include/mlir/Analysis/DataFlow/ConstantPropagationAnalysis.h"  // from @llvm-project
+#include "mlir/include/mlir/Analysis/DataFlow/DeadCodeAnalysis.h"  // from @llvm-project
+#include "mlir/include/mlir/Analysis/DataFlowFramework.h"  // from @llvm-project
+#include "mlir/include/mlir/Dialect/Func/IR/FuncOps.h"     // from @llvm-project
+#include "mlir/include/mlir/IR/Operation.h"                // from @llvm-project
+#include "mlir/include/mlir/IR/Value.h"                    // from @llvm-project
+#include "mlir/include/mlir/IR/Visitors.h"                 // from @llvm-project
+#include "mlir/include/mlir/Support/LLVM.h"                // from @llvm-project
+#include "mlir/include/mlir/Transforms/Passes.h"           // from @llvm-project
 
 #define DEBUG_TYPE "PopulateScale"
 
@@ -29,9 +33,9 @@ struct PopulateScale : impl::PopulateScaleBase<PopulateScale> {
   using PopulateScaleBase::PopulateScaleBase;
 
   void runOnOperation() override {
-    auto bgvSchemeParam = mlir::dyn_cast<bgv::SchemeParamAttr>(
+    auto bgvSchemeParamAttr = mlir::dyn_cast<bgv::SchemeParamAttr>(
         getOperation()->getAttr(bgv::BGVDialect::kSchemeParamAttrName));
-    auto Q = bgvSchemeParam.getQ();
+    auto Q = bgvSchemeParamAttr.getQ();
     std::vector<int64_t> qi;
     qi.reserve(Q.size());
     for (int i = 0; i < Q.size(); i++) {
@@ -40,7 +44,7 @@ struct PopulateScale : impl::PopulateScaleBase<PopulateScale> {
 
     auto L = Q.size() - 1;
 
-    auto t = bgvSchemeParam.getPlaintextModulus();
+    auto t = bgvSchemeParamAttr.getPlaintextModulus();
 
     std::vector<int64_t> scale(L + 1, 0);
 
@@ -59,6 +63,12 @@ struct PopulateScale : impl::PopulateScaleBase<PopulateScale> {
       scale[i] = newScale;
     }
 
+    // for arith.mul, the result scale is scale big
+    std::vector<int64_t> scaleBig(L + 1, 0);
+    for (auto i = 0; i <= L; i++) {
+      scaleBig[i] = scale[i] * scale[i] % t;
+    }
+
     LLVM_DEBUG({
       llvm::dbgs() << "PopulateScale: scale = [";
       for (long i : scale) {
@@ -67,14 +77,25 @@ struct PopulateScale : impl::PopulateScaleBase<PopulateScale> {
       llvm::dbgs() << "]\n";
     });
 
+    LLVM_DEBUG({
+      llvm::dbgs() << "PopulateScale: scaleBig = [";
+      for (long i : scaleBig) {
+        llvm::dbgs() << i << ", ";
+      }
+      llvm::dbgs() << "]\n";
+    });
+
     // now we calculate the scale required for adjust_scale
-    // adjust_scale[i] = scale[i - 1] * q[i] mod t
-    // which means adjust_scale[i] * q[i]^-1 = scale[i - 1] mod t
+    // adjust_scale[i] = scale[i - 1] * scale[i - 1] * q[i] mod t
+    // which means adjust_scale[i] * q[i]^-1 = scale[i - 1] * scale[i - 1] mod t
+    // this is for level i - 1, the majority of the scale is scale[i - 1] *
+    // scale[i - 1] namely scaleDeg = 2
+    // this is highly coupled with secret-insert-mgmt-bgv!
     std::vector<int64_t> adjustScale(L + 1, 0);
     adjustScale[0] = 1;
     for (auto i = 1; i <= L; i++) {
       auto q = qi[i] % t;
-      auto newScale = (adjustScale[i - 1] * q) % t;
+      auto newScale = ((scale[i - 1] * scale[i - 1] % t) * q) % t;
       adjustScale[i] = newScale;
     }
 
@@ -98,6 +119,20 @@ struct PopulateScale : impl::PopulateScaleBase<PopulateScale> {
           "scale",
           IntegerAttr::get(IntegerType::get(op.getContext(), 64), newScale));
     });
+
+    DataFlowSolver solver;
+    solver.load<dataflow::DeadCodeAnalysis>();
+    solver.load<dataflow::SparseConstantPropagation>();
+    // ScaleAnalysis depends on SecretnessAnalysis
+    solver.load<SecretnessAnalysis>();
+    solver.load<ScaleAnalysis>(
+        bgv::SchemeParam::getSchemeParamFromAttr(bgvSchemeParamAttr));
+
+    if (failed(solver.initializeAndRun(getOperation()))) {
+      getOperation()->emitOpError() << "Failed to run the analysis.\n";
+      signalPassFailure();
+      return;
+    }
   }
 };
 
