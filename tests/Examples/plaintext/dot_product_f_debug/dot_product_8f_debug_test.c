d785f7304b3cd69ac6d362e30f0a7659d65893af
Zenithal <i@zenithal.me>
2025-03-16 13:48:45 +0000

    plaintext-backend: export log for other uses

--- tests/Examples/plaintext/dot_product_f_debug/dot_product_8f_debug_test.c
+++ tests/Examples/plaintext/dot_product_f_debug/dot_product_8f_debug_test.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
+FILE *output;
+
 // This is the function we want to call from LLVM
 float dot_product(
     /* arg 0*/
@@ -17,14 +19,25 @@ void __heir_debug_tensor_8xf32_(
     float *allocated, float *aligned, int64_t offset, int64_t size,
     int64_t stride) {
   for (int i = 0; i < size; i++) {
-    fprintf(stderr, "%.15f ", *(aligned + i * stride));
+    fprintf(output, "%.15f ", *(aligned + i * stride));
   }
-  fprintf(stderr, "\n");
+  fprintf(output, "\n");
 }
 
-void __heir_debug_f32(float value) { fprintf(stderr, "%.15f \n", value); }
+void __heir_debug_f32(float value) { fprintf(output, "%.15f \n", value); }
 
-int main() {
+int main(int argc, char **argv) {
+  // the first argument is the output file
+  if (argc > 1) {
+    output = fopen(argv[1], "w");
+    if (output == NULL) {
+      fprintf(stderr, "Error opening file\n");
+      return 1;
+    }
+  } else {
+    output = stderr;
+  }
+
   float arg0[8] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8};
   float arg1[8] = {0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9};
   float expected = 2.5;
@@ -37,7 +50,13 @@ int main() {
 
   if (fabs(res - expected) < 1e-3) {
     printf("Test passed\n");
+#ifdef EXPECT_FAILURE
+    return 1;
+#endif
   } else {
     printf("Test failed %f != %f\n", res, expected);
+#ifndef EXPECT_FAILURE
+    return 1;
+#endif
   }
 }
