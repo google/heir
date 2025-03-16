8c99ed7e8359233a1173101a3536b81a995c1d8d
Jeremy Kun <j2kun@users.noreply.github.com>
2025-03-16 14:48:20 -0700

    break cyclic dep btw Polynomial and ModArith for attrs

--- lib/Dialect/Polynomial/IR/PolynomialAttributes.h
+++ lib/Dialect/Polynomial/IR/PolynomialAttributes.h
@@ -1,8 +1,10 @@
 #ifndef LIB_DIALECT_POLYNOMIAL_IR_POLYNOMIALATTRIBUTES_H_
 #define LIB_DIALECT_POLYNOMIAL_IR_POLYNOMIALATTRIBUTES_H_
 
+// IWYU pragma: begin_keep
 #include "lib/Dialect/Polynomial/IR/PolynomialDialect.h"
 #include "lib/Utils/Polynomial/Polynomial.h"
+// IWYU pragma: end_keep
 
 #define GET_ATTRDEF_CLASSES
 #include "lib/Dialect/Polynomial/IR/PolynomialAttributes.h.inc"
--- lib/Dialect/Polynomial/IR/PolynomialDialect.h
+++ lib/Dialect/Polynomial/IR/PolynomialDialect.h
@@ -1,10 +1,12 @@
 #ifndef LIB_DIALECT_POLYNOMIAL_IR_POLYNOMIALDIALECT_H_
 #define LIB_DIALECT_POLYNOMIAL_IR_POLYNOMIALDIALECT_H_
 
+// IWYU pragma: begin_keep
 #include "mlir/include/mlir/IR/Builders.h"               // from @llvm-project
 #include "mlir/include/mlir/IR/BuiltinTypes.h"           // from @llvm-project
 #include "mlir/include/mlir/IR/Dialect.h"                // from @llvm-project
 #include "mlir/include/mlir/IR/DialectImplementation.h"  // from @llvm-project
+// IWYU pragma: end_keep
 
 // Generated headers (block clang-format from messing up order)
 #include "lib/Dialect/Polynomial/IR/PolynomialDialect.h.inc"
