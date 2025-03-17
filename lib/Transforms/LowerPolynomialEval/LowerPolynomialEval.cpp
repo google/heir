ce7bed9bec5f7b5659ddaa41dbd44206729bc672
Jeremy Kun <j2kun@users.noreply.github.com>
2025-03-16 21:22:53 -0700

    add pass options and pattern shells

--- lib/Transforms/LowerPolynomialEval/LowerPolynomialEval.cpp
+++ lib/Transforms/LowerPolynomialEval/LowerPolynomialEval.cpp
@@ -17,38 +17,126 @@ namespace heir {
 #define GEN_PASS_DEF_LOWERPOLYNOMIALEVAL
 #include "lib/Transforms/LowerPolynomialEval/LowerPolynomialEval.h.inc"
 
+struct LoweringBase : public OpRewritePattern<polynomial::EvalOp> {
+  LoweringBase(mlir::MLIRContext *context, bool force = false,
+               const std::string &dialect = "")
+      : mlir::OpRewritePattern<polynomial::EvalOp>(context),
+        force(force),
+        dialect(dialect) {}
+
+  Dialect *getDialect(polynomial::EvalOp op) const {
+    PolynomialEvalInterface evalInterface(op.getContext());
+    return dialect.empty() ? &op.getValue().getType().getDialect()
+                           : op.getContext()->getOrLoadDialect(dialect);
+  }
+
+ private:
+  // Force the use of this pattern, ignoring any heuristics on whether to apply
+  // it.
+  const bool force;
+
+  // A dialect override provided via flag to use with the
+  // PolynomialEvalInterface.
+  const std::string dialect;
+};
+
+struct LowerViaHorner : public LoweringBase {
+  using LoweringBase::LoweringBase;
+
+  LogicalResult matchAndRewrite(polynomial::EvalOp op,
+                                PatternRewriter &rewriter) const override {
+    Dialect *dialect = getDialect(op);
+    PolynomialEvalInterface interface(op.getContext());
+
+    // FIXME: if force, ignore heuristics on whether to use this method
+    // FIXME: add lowering
+
+    return success();
+  }
+};
+
+struct LowerViaPatersonStockmeyerMonomial : public LoweringBase {
+  using LoweringBase::LoweringBase;
+
+  LogicalResult matchAndRewrite(polynomial::EvalOp op,
+                                PatternRewriter &rewriter) const override {
+    Dialect *dialect = getDialect(op);
+    PolynomialEvalInterface interface(op.getContext());
+
+    // FIXME: if force, ignore heuristics on whether to use this method
+    // FIXME: add lowering
+
+    return success();
+  }
+};
+
+struct LowerViaClenshaw : public LoweringBase {
+  using LoweringBase::LoweringBase;
+
+  LogicalResult matchAndRewrite(polynomial::EvalOp op,
+                                PatternRewriter &rewriter) const override {
+    Dialect *dialect = getDialect(op);
+    PolynomialEvalInterface interface(op.getContext());
+    return failure();
+  }
+};
+
+struct LowerViaPatersonStockmeyerChebyshev : public LoweringBase {
+  using LoweringBase::LoweringBase;
+
+  LogicalResult matchAndRewrite(polynomial::EvalOp op,
+                                PatternRewriter &rewriter) const override {
+    Dialect *dialect = getDialect(op);
+    PolynomialEvalInterface interface(op.getContext());
+    return failure();
+  }
+};
+
+struct LowerViaBabyStepGiantStep : public LoweringBase {
+  using LoweringBase::LoweringBase;
+
+  LogicalResult matchAndRewrite(polynomial::EvalOp op,
+                                PatternRewriter &rewriter) const override {
+    Dialect *dialect = getDialect(op);
+    PolynomialEvalInterface interface(op.getContext());
+    return failure();
+  }
+};
+
 struct LowerPolynomialEval
     : impl::LowerPolynomialEvalBase<LowerPolynomialEval> {
   using LowerPolynomialEvalBase::LowerPolynomialEvalBase;
-
-  // FIXME: replace with actual lowering patterns
-  struct DebugPrintPattern : public OpRewritePattern<polynomial::EvalOp> {
-    DebugPrintPattern(mlir::MLIRContext *context)
-        : mlir::OpRewritePattern<polynomial::EvalOp>(context) {}
-
-    LogicalResult matchAndRewrite(polynomial::EvalOp op,
-                                  PatternRewriter &rewriter) const override {
-      LLVM_DEBUG(llvm::dbgs() << "Lowering polynomial::EvalOp: " << op << "\n");
-
-      PolynomialEvalInterface evalInterface(op.getContext());
-      Dialect *valueDialect = &op.getValue().getType().getDialect();
-      if (!evalInterface.supportsPolynomial(op.getPolynomial(), valueDialect)) {
-        return op.emitOpError()
-               << "dialect for input type " << op.getValue().getType()
-               << " does not support polynomial " << op.getPolynomial();
-      }
-
-      rewriter.modifyOpInPlace(
-          op, [&]() { op->setAttr("lowered", rewriter.getUnitAttr()); });
-      return success();
-    }
-  };
 
   void runOnOperation() override {
     MLIRContext *context = &getContext();
     RewritePatternSet patterns(context);
 
-    patterns.add<DebugPrintPattern>(context);
+    if (method.hasValue() && !method.empty()) {
+      if (method == "horner") {
+        patterns.add<LowerViaHorner>(context, /*force=*/true, dialect);
+      } else if (method == "ps") {
+        patterns.add<LowerViaPatersonStockmeyerMonomial>(
+            context, /*force=*/true, dialect);
+      } else if (method == "clenshaw") {
+        patterns.add<LowerViaClenshaw>(context, /*force=*/true, dialect);
+      } else if (method == "ps-cheb") {
+        patterns.add<LowerViaPatersonStockmeyerChebyshev>(
+            context, /*force=*/true, dialect);
+      } else if (method == "bsgs") {
+        patterns.add<LowerViaBabyStepGiantStep>(context, /*force=*/true,
+                                                dialect);
+      } else {
+        getOperation()->emitError() << "Unknown lowering method: " << method;
+        signalPassFailure();
+        return;
+      }
+    } else {
+      patterns.add<LowerViaHorner, LowerViaBabyStepGiantStep,
+                   LowerViaPatersonStockmeyerChebyshev, LowerViaClenshaw,
+                   LowerViaPatersonStockmeyerMonomial>(context,
+                                                       /*force=*/false,
+                                                       dialect);
+    }
 
     walkAndApplyPatterns(getOperation(), std::move(patterns));
   }
