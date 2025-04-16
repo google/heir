a06532b1aeae5b432ad5b7d4ae62052d037600fd
Asra Ali <asraa@google.com>
2025-04-16 09:04:50 -0700

    [draft] mlir-to-cggi: allow for non-yosys compilation
    Fixes https://github.com/google/heir/issues/1646
    
    The current issues with using the non-Yosys pipeline seem to be:
    * Lack of support for memref/tensor inputs for quart int?
    * Does not utilize the secretness analysis for plaintext vs ciphertext operations
    
    PiperOrigin-RevId: 748313088

--- lib/Pipelines/BooleanPipelineRegistration.h
+++ lib/Pipelines/BooleanPipelineRegistration.h
@@ -4,15 +4,64 @@
 #include <functional>
 #include <string>
 
-#include "lib/Transforms/YosysOptimizer/YosysOptimizer.h"
 #include "llvm/include/llvm/Support/CommandLine.h"  // from @llvm-project
 #include "mlir/include/mlir/Pass/PassManager.h"     // from @llvm-project
 #include "mlir/include/mlir/Pass/PassOptions.h"     // from @llvm-project
 #include "mlir/include/mlir/Pass/PassRegistry.h"    // from @llvm-project
 
+#ifndef HEIR_NO_YOSYS
+#include "lib/Transforms/YosysOptimizer/YosysOptimizer.h"
+#endif
+
 namespace mlir::heir {
 
-struct MLIRToCGGIPipelineOptions : public YosysOptimizerPipelineOptions {};
+enum DataType { Bool, Shortint, Integer };
+
+#ifndef HEIR_NO_YOSYS
+// If Yosys is enabled, also add all yosys optimizer pipeline options.
+struct MLIRToCGGIPipelineOptions : public YosysOptimizerPipelineOptions {
+  PassOptions::Option<enum DataType> dataType{
+      *this, "data-type",
+      llvm::cl::desc("Data type to use for arithmetization, yosys must be "
+                     "enabled for Boolean."),
+      llvm::cl::init(Bool),
+      llvm::cl::values(
+          clEnumVal(Bool, "booleanize with Yosys"),
+          clEnumVal(Shortint, "decompose operations into 8 bit data types"),
+          clEnumVal(Integer, "decompose operations into 32 bit data types"))};
+};
+
+using CGGIPipelineBuilder =
+    std::function<void(OpPassManager &, const MLIRToCGGIPipelineOptions &)>;
+
+CGGIPipelineBuilder mlirToCGGIPipelineBuilder(const std::string &yosysFilesPath,
+                                              const std::string &abcPath);
+
+void mlirToCGGIPipeline(OpPassManager &pm,
+                        const MLIRToCGGIPipelineOptions &options,
+                        const std::string &yosysFilesPath,
+                        const std::string &abcPath);
+
+#else
+struct MLIRToCGGIPipelineOptions
+    : public PassPipelineOptions<MLIRToCGGIPipelineOptions> {
+  PassOptions::Option<enum DataType> dataType{
+      *this, "data-type",
+      llvm::cl::desc("Data type to use for arithmetization."),
+      llvm::cl::init(Shortint),
+      llvm::cl::values(
+          clEnumVal(Shortint, "decompose operations into 8 bit data types"),
+          clEnumVal(Integer, "decompose operations into 32 bit data types"))};
+};
+
+using CGGIPipelineBuilder =
+    std::function<void(OpPassManager &, const MLIRToCGGIPipelineOptions &)>;
+
+CGGIPipelineBuilder mlirToCGGIPipelineBuilder();
+
+void mlirToCGGIPipeline(OpPassManager &pm,
+                        const MLIRToCGGIPipelineOptions &options);
+#endif
 
 struct CGGIBackendOptions : public PassPipelineOptions<CGGIBackendOptions> {
   PassOptions::Option<int> parallelism{
@@ -23,21 +72,10 @@ struct CGGIBackendOptions : public PassPipelineOptions<CGGIBackendOptions> {
       llvm::cl::init(-1)};
 };
 
-using CGGIPipelineBuilder =
-    std::function<void(OpPassManager &, const MLIRToCGGIPipelineOptions &)>;
-
 using CGGIBackendPipelineBuilder = std::function<void(OpPassManager &)>;
 
 using JaxiteBackendPipelineBuilder =
     std::function<void(OpPassManager &, const CGGIBackendOptions &)>;
-
-CGGIPipelineBuilder mlirToCGGIPipelineBuilder(const std::string &yosysFilesPath,
-                                              const std::string &abcPath);
-
-void mlirToCGGIPipeline(OpPassManager &pm,
-                        const MLIRToCGGIPipelineOptions &options,
-                        const std::string &yosysFilesPath,
-                        const std::string &abcPath);
 
 CGGIBackendPipelineBuilder toTfheRsPipelineBuilder();
 
