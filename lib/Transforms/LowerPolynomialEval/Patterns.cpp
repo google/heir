9ef55daa871690ec14d90bead91a79ca6d3fcd31
Vadym Doroshenko <dvadym@google.com>
2025-05-05 09:46:26 -0700

    [HEIR] Lower PolynomialEval via Chebyshev decomposition.
    Implemented lowering for eval op via Chebyshev decomposition. This lowering uses Chebyshev decomposition of the polynomial coefficients and Paterson-Stockmeyer algorithm for evaluation.
    
    PiperOrigin-RevId: 754971159

--- lib/Transforms/LowerPolynomialEval/Patterns.cpp
+++ lib/Transforms/LowerPolynomialEval/Patterns.cpp
@@ -7,7 +7,9 @@
 
 #include "lib/Dialect/Polynomial/IR/PolynomialAttributes.h"
 #include "lib/Dialect/Polynomial/IR/PolynomialOps.h"
+#include "lib/Utils/Polynomial/ChebyshevDecomposition.h"
 #include "lib/Utils/Polynomial/Polynomial.h"
+#include "llvm/include/llvm/ADT/APFloat.h"             // from @llvm-project
 #include "llvm/include/llvm/ADT/TypeSwitch.h"          // from @llvm-project
 #include "llvm/include/llvm/Support/Debug.h"           // from @llvm-project
 #include "mlir/include/mlir/Dialect/Arith/IR/Arith.h"  // from @llvm-project
@@ -29,6 +31,35 @@ namespace heir {
 using polynomial::EvalOp;
 using polynomial::FloatPolynomial;
 using polynomial::TypedFloatPolynomialAttr;
+
+namespace {
+std::vector<Value> getPowers(Value x, int64_t k, ImplicitLocOpBuilder& b) {
+  std::vector<Value> xPowers(k + 1);
+  xPowers[0] =
+      b.create<arith::ConstantOp>(x.getType(), b.getOneAttr(x.getType()));
+  xPowers[1] = x;
+  for (int64_t i = 2; i <= k; i++) {
+    if (i % 2 == 0) {
+      // x^{2k} = (x^{k})^2
+      xPowers[i] =
+          b.create<arith::MulFOp>(xPowers[i / 2], xPowers[i / 2]).getResult();
+    } else {
+      // x^{2k+1} = x^{k}x^{k+1}
+      xPowers[i] = b.create<arith::MulFOp>(xPowers[i / 2], xPowers[i / 2 + 1])
+                       .getResult();
+    }
+  }
+  return xPowers;
+}
+
+bool hasElementsLargerThan(const SmallVector<APFloat>& v, APFloat threshold) {
+  for (APFloat a : v) {
+    if (llvm::abs(a) >= threshold) return true;
+  }
+  return false;
+}
+
+}  // namespace
 
 TypedAttr getScalarOrDenseAttr(Type tensorOrScalarType, APFloat value) {
   return TypeSwitch<Type, TypedAttr>(tensorOrScalarType)
@@ -210,7 +241,94 @@ LogicalResult LowerViaPatersonStockmeyerMonomial::matchAndRewrite(
 
 LogicalResult LowerViaPatersonStockmeyerChebyshev::matchAndRewrite(
     EvalOp op, PatternRewriter& rewriter) const {
-  return failure();
+  // TODO(dvadym): add support for not [-1, 1] range.
+  Type evaluatedType = op.getValue().getType();
+  ImplicitLocOpBuilder b(op.getLoc(), rewriter);
+  b.setInsertionPoint(op);
+
+  auto attr =
+      dyn_cast<polynomial::TypedFloatPolynomialAttr>(op.getPolynomialAttr());
+  if (!attr) return failure();
+
+  FloatPolynomial polynomial = attr.getValue().getPolynomial();
+  auto terms = polynomial.getTerms();
+
+  int64_t maxDegree = terms.back().getExponent().getSExtValue();
+
+  auto monomialMap = attr.getValue().getPolynomial().getCoeffMap();
+  llvm::SmallVector<APFloat> coeffs(maxDegree + 1, APFloat(0.0));
+  for (auto& [key, monomial] : monomialMap) {
+    coeffs[key] = monomial.getCoefficient();
+  }
+
+  // Choose k optimally - sqrt of maxDegree is typically a good choice
+  int64_t k = std::max(static_cast<int64_t>(std::ceil(std::sqrt(maxDegree))),
+                       static_cast<int64_t>(1));
+
+  // Precompute T_0(x), T_1(x), ..., T_k(x).
+  Value x = op.getOperand();
+  std::vector<Value> chebPolynomialValues(k + 1);
+  chebPolynomialValues[0] =
+      b.create<arith::ConstantOp>(evaluatedType, b.getOneAttr(evaluatedType));
+  chebPolynomialValues[1] = x;
+  auto number2 = b.create<arith::ConstantOp>(
+      evaluatedType, b.getIntegerAttr(evaluatedType, 2));
+  for (int64_t i = 2; i <= k; i++) {
+    if (i % 2 == 0) {
+      // T_{2n}(x) = 2(T_n(x))^2 - 1
+      auto tnSquared = b.create<arith::MulFOp>(chebPolynomialValues[i / 2],
+                                               chebPolynomialValues[i / 2]);
+      auto tnSquaredMul2 = b.create<arith::MulFOp>(tnSquared, number2);
+      chebPolynomialValues[i] =
+          b.create<arith::SubFOp>(tnSquaredMul2, chebPolynomialValues[0]);
+    } else {
+      // T_{2n+1}(x) = 2*T_n(x) * T_{n+1}(x) - x
+      auto tnMulTnPlusOne = b.create<arith::MulFOp>(
+          chebPolynomialValues[i / 2], chebPolynomialValues[i / 2 + 1]);
+      auto tnMulTnPlusOneMul2 =
+          b.create<arith::MulFOp>(tnMulTnPlusOne, number2);
+      chebPolynomialValues[i] = b.create<arith::SubFOp>(tnMulTnPlusOneMul2, x);
+    }
+  }
+
+  // Precompute (T_k)^0, T_k^1, ..., T_k^l
+  std::vector<Value> chebKPolynomialPowers =
+      getPowers(chebPolynomialValues.back(), k, b);
+
+  polynomial::ChebyshevDecomposition decomposition =
+      polynomial::decompose(coeffs, k);
+
+  Value result = nullptr;
+  bool resultEmpty = true;
+  const APFloat kMinCoeffs = APFloat(1e-12);
+  for (int i = 0; i < decomposition.coeffs.size(); ++i) {
+    if (!hasElementsLargerThan(decomposition.coeffs[i], kMinCoeffs)) continue;
+    Value pol = nullptr;
+    bool polEmpty = true;
+    for (int j = 0; j < decomposition.coeffs.size(); ++j) {
+      APFloat coeff = decomposition.coeffs[i][j];
+      if (llvm::abs(coeff) < kMinCoeffs) continue;
+      auto coef = b.create<arith::ConstantOp>(
+          evaluatedType,
+          b.getFloatAttr(evaluatedType, decomposition.coeffs[i][j]));
+      auto term = b.create<arith::MulFOp>(coef, chebPolynomialValues[j]);
+      if (polEmpty) {
+        pol = term;
+        polEmpty = false;
+      } else {
+        pol = b.create<arith::AddFOp>(pol, term);
+      }
+    }
+    pol = b.create<arith::MulFOp>(pol, chebKPolynomialPowers[i]);
+    if (!resultEmpty) {
+      result = pol;
+      resultEmpty = false;
+    } else {
+      result = b.create<arith::AddFOp>(result, pol);
+    }
+  }
+  rewriter.replaceOp(op, result);
+  return success();
 }
 
 }  // namespace heir
