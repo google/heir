8ba4ced3c3286c186793fdbee92815275cef92de
Jeremy Kun <j2kun@users.noreply.github.com>
2025-05-14 14:42:45 -0700

    checkpoint

--- lib/Dialect/Secret/Conversions/SecretToBGV/SecretToBGV.cpp
+++ lib/Dialect/Secret/Conversions/SecretToBGV/SecretToBGV.cpp
@@ -159,6 +159,39 @@ LogicalResult disallowFloatlike(const Type &type) {
   return success();
 }
 
+// Client helper functions require mgmt attributes in order for the type
+// converter to convert them. The correct mgmt attribute to use is
+// determined by the function the helpers are made for, and so
+// this method copies the relevant mgmt attributes from
+// the original function to the client helper function arg/result attrs
+// so they can be propagated through those IRs. Any existing mgmt attributes
+// on the client helpers are ignored.
+//
+// E.g., given the following functions
+//
+//   func.func @foo(
+//      %arg0: !secret.secret<tensor<8xi16>> {mgmt.mgmt = ...})
+//         -> (!secret.secret<tensor<8xi16>> {mgmt.mgmt = ...}) {
+//     ...
+//   }
+//   func.func @foo__encrypt__arg0(
+//      %arg0: tensor<8xi16>) -> !secret.secret<tensor<8xi16>>
+//      attributes {client_enc_func = {func_name = "foo", index = 0 : i64}} {
+//     ...
+//   }
+//   func.func @foo__decrypt__result0(
+//      %arg0: !secret.secret<tensor<8xi16>>) -> i16
+//       attributes {client_dec_func = {func_name = "foo", index = 0 : i64}} {
+//     ...
+//   }
+//
+// The encrypt function needs its result annotated with a mgmt attr that
+// matches the mgmt attr of @foo's 0th argument, and the decrypt function's
+// argument needs a mgmt attr that matches the mgmt attr of @foo's 0th result.
+// Then the mgmt attr needs to be backward propagated in the encryption
+// function and forward propagated in the decryption function.
+void copyMgmtAttrToClientHelpers(Operation *op) {}
+
 struct SecretToBGV : public impl::SecretToBGVBase<SecretToBGV> {
   using SecretToBGVBase::SecretToBGVBase;
 
@@ -243,6 +276,8 @@ struct SecretToBGV : public impl::SecretToBGVBase<SecretToBGV> {
       signalPassFailure();
       return;
     }
+
+    copyMgmtAttrToClientHelpers(module);
 
     // Invariant: for every SecretType, there is a corresponding MgmtAttr
     // attached to it, either in its DefiningOp or getOwner()->getParentOp()
