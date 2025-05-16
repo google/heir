91cbd96511ba36270f79d944a9140214ab1108a6
Zenithal <i@zenithal.me>
2025-05-16 13:02:13 +0000

    SelectVariableNames: Make a Wrapper for DataflowAnalysis

--- lib/Analysis/SelectVariableNames/SelectVariableNames.h
+++ lib/Analysis/SelectVariableNames/SelectVariableNames.h
@@ -4,14 +4,53 @@
 #include <cassert>
 #include <string>
 
-#include "llvm/include/llvm/ADT/DenseMap.h"          // from @llvm-project
-#include "mlir/include/mlir/IR/BuiltinAttributes.h"  // from @llvm-project
-#include "mlir/include/mlir/IR/Operation.h"          // from @llvm-project
-#include "mlir/include/mlir/IR/Value.h"              // from @llvm-project
-#include "mlir/include/mlir/Support/LLVM.h"          // from @llvm-project
+#include "llvm/include/llvm/ADT/DenseMap.h"  // from @llvm-project
+#include "mlir/include/mlir/Analysis/DataFlow/SparseAnalysis.h"  // from @llvm-project
+#include "mlir/include/mlir/Analysis/DataFlowFramework.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinAttributes.h"        // from @llvm-project
+#include "mlir/include/mlir/IR/Operation.h"                // from @llvm-project
+#include "mlir/include/mlir/IR/Value.h"                    // from @llvm-project
+#include "mlir/include/mlir/Support/LLVM.h"                // from @llvm-project
 
 namespace mlir {
 namespace heir {
+
+class VariableName {
+ public:
+  VariableName() = default;
+  VariableName(const std::string &name, int integer)
+      : initialized(true), name(name), integer(integer) {}
+  ~VariableName() = default;
+
+  bool operator==(const VariableName &other) const {
+    return initialized == other.initialized && name == other.name &&
+           integer == other.integer;
+  }
+
+  // For Lattice in DataFlowFramework
+  static VariableName join(const VariableName &lhs, const VariableName &rhs) {
+    if (!lhs.isInitialized() || lhs.getName().empty()) return rhs;
+    if (!rhs.isInitialized() || rhs.getName().empty()) return lhs;
+
+    if (lhs.getName() == rhs.getName()) return lhs;
+
+    // If the names are different, we can just return the first one
+    return lhs;
+  }
+
+  void print(raw_ostream &os) const {
+    os << "Name: " << name << " Int: " << integer;
+  }
+
+  bool isInitialized() const { return initialized; }
+  std::string getName() const { return name; }
+  int getInteger() const { return integer; }
+
+ private:
+  bool initialized = false;
+  std::string name;
+  int integer;
+};
 
 class SelectVariableNames {
  public:
@@ -22,31 +61,66 @@ class SelectVariableNames {
   /// value was not assigned a name (suggesting the value was not in the IR
   /// tree that this class was constructed with).
   std::string getNameForValue(Value value) const {
-    assert(variableNames.contains(value));
-    return variableNames.lookup(value);
+    return lookup(value).getName();
   }
 
-  // Return the unique integer assigned to a given value.
-  int getIntForValue(Value value) const {
-    assert(variableToInteger.contains(value));
-    return variableToInteger.lookup(value);
+  /// Return the unique integer assigned to a given value.
+  int getIntForValue(Value value) const { return lookup(value).getInteger(); }
+
+  /// returns the internal VariableName struct
+  VariableName lookup(Value value) const {
+    assert(variableNames.contains(value));
+    return variableNames.lookup(value);
   }
 
   // Map from one value's name to another value's name.
   void mapValueNameToValue(Value fromValue, Value toValue) {
     assert(variableNames.contains(toValue));
     variableNames[fromValue] = variableNames[toValue];
-    variableToInteger[fromValue] = variableToInteger[toValue];
   }
 
   bool contains(Value value) const { return variableNames.contains(value); }
 
  private:
-  std::string suggestNameForValue(Value value);
+  std::string suggestPrefixForValue(Value value);
 
   std::string defaultPrefix{"v"};
-  llvm::DenseMap<Value, std::string> variableNames;
-  llvm::DenseMap<Value, int> variableToInteger;
+  llvm::DenseMap<Value, VariableName> variableNames;
+};
+
+//===----------------------------------------------------------------------===//
+// DataFlowAnalysis Wrapper
+//===----------------------------------------------------------------------===//
+
+class VariableNameLattice : public dataflow::Lattice<VariableName> {
+ public:
+  using Lattice::Lattice;
+};
+
+/// This analysis does not update the Lattice when IR changes!
+class SelectVariableNameAnalysis
+    : public dataflow::SparseForwardDataFlowAnalysis<VariableNameLattice> {
+ public:
+  explicit SelectVariableNameAnalysis(
+      DataFlowSolver &solver, const SelectVariableNames &selectVariableNames)
+      : SparseForwardDataFlowAnalysis(solver),
+        selectVariableNames(selectVariableNames) {}
+
+  void setToEntryState(VariableNameLattice *lattice) override {
+    propagateIfChanged(
+        lattice,
+        lattice->join(selectVariableNames.lookup(lattice->getAnchor())));
+  }
+
+  // does nothing
+  LogicalResult visitOperation(
+      Operation *operation, ArrayRef<const VariableNameLattice *> operands,
+      ArrayRef<VariableNameLattice *> results) override {
+    return success();
+  };
+
+ private:
+  SelectVariableNames selectVariableNames;
 };
 
 }  // namespace heir
--- lib/Transforms/ValidateNoise/ValidateNoise.cpp
+++ lib/Transforms/ValidateNoise/ValidateNoise.cpp
@@ -14,6 +14,7 @@
 #include "lib/Analysis/NoiseAnalysis/BGV/NoiseCanEmbModel.h"
 #include "lib/Analysis/NoiseAnalysis/NoiseAnalysis.h"
 #include "lib/Analysis/SecretnessAnalysis/SecretnessAnalysis.h"
+#include "lib/Analysis/SelectVariableNames/SelectVariableNames.h"
 #include "lib/Dialect/BGV/IR/BGVAttributes.h"
 #include "lib/Dialect/BGV/IR/BGVDialect.h"
 #include "lib/Dialect/Mgmt/IR/MgmtOps.h"
@@ -172,6 +173,9 @@ struct ValidateNoise : impl::ValidateNoiseBase<ValidateNoise> {
     solver.load<dataflow::SparseConstantPropagation>();
     // NoiseAnalysis depends on SecretnessAnalysis
     solver.load<SecretnessAnalysis>();
+    // NoiseAnalysis depends on SelectVariableNameAnalysis
+    SelectVariableNames selectVariableNames(getOperation());
+    solver.load<SelectVariableNameAnalysis>(selectVariableNames);
 
     solver.load<NoiseAnalysis<NoiseModel>>(schemeParam, model);
 
