7684cfc2df84fa5cef38da02d55f1be9fd80839a
Jeremy Kun <j2kun@users.noreply.github.com>
2025-06-04 17:12:17 -0700

    Lower secret.cast in secret-forget-secrets

--- lib/Utils/TransformUtils.cpp
+++ lib/Utils/TransformUtils.cpp
@@ -4,9 +4,11 @@
 #include <string>
 #include <string_view>
 
-#include "mlir/include/mlir/Dialect/Func/IR/FuncOps.h"  // from @llvm-project
-#include "mlir/include/mlir/IR/BuiltinOps.h"            // from @llvm-project
-#include "mlir/include/mlir/IR/Visitors.h"              // from @llvm-project
+#include "mlir/include/mlir/Dialect/Arith/IR/Arith.h"    // from @llvm-project
+#include "mlir/include/mlir/Dialect/Func/IR/FuncOps.h"   // from @llvm-project
+#include "mlir/include/mlir/Dialect/MemRef/IR/MemRef.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinOps.h"             // from @llvm-project
+#include "mlir/include/mlir/IR/Visitors.h"               // from @llvm-project
 
 namespace mlir {
 namespace heir {
@@ -47,6 +49,55 @@ func::FuncOp detectEntryFunction(ModuleOp moduleOp,
         "options");
   }
   return entryFunc;
+}
+
+Value convertIntegerValueToMemrefOfBits(Value integer, OpBuilder &b,
+                                        Location loc) {
+  IntegerType argType = mlir::cast<IntegerType>(integer.getType());
+  int width = argType.getWidth();
+  if (width == 1) {
+    return integer;
+  }
+
+  auto allocOp =
+      b.create<memref::AllocOp>(loc, MemRefType::get({width}, b.getI1Type()));
+  for (int i = 0; i < width; i++) {
+    // These arith ops correspond to extracting the i-th bit
+    // from the input
+    auto shiftAmount =
+        b.create<arith::ConstantOp>(loc, argType, b.getIntegerAttr(argType, i));
+    auto bitMask = b.create<arith::ConstantOp>(
+        loc, argType, b.getIntegerAttr(argType, 1 << i));
+    auto andOp = b.create<arith::AndIOp>(loc, integer, bitMask);
+    auto shifted = b.create<arith::ShRSIOp>(loc, andOp, shiftAmount);
+    b.create<memref::StoreOp>(
+        loc, b.create<arith::TruncIOp>(loc, b.getI1Type(), shifted), allocOp,
+        ValueRange{b.create<arith::ConstantIndexOp>(loc, i)});
+  }
+
+  return allocOp.getResult();
+}
+
+Value convertMemrefOfBitsToInteger(Value memref, Type resultType, OpBuilder &b,
+                                   Location loc) {
+  auto memrefType = cast<MemRefType>(memref.getType());
+  auto integerType = cast<IntegerType>(resultType);
+  assert(memrefType.getRank() == 1 && "Expected memref of bits to be 1D");
+
+  Value result =
+      b.create<arith::ConstantIntOp>(loc, 0, integerType).getResult();
+  for (int i = 0; i < memrefType.getNumElements(); i++) {
+    // The i-th bit of the memref is stored at bit position i
+    auto loadOp = b.create<memref::LoadOp>(
+        loc, memref, ValueRange{b.create<arith::ConstantIndexOp>(loc, i)});
+    auto extOp = b.create<arith::ExtSIOp>(loc, integerType, loadOp.getResult());
+    auto shiftAmount = b.create<arith::ConstantIntOp>(loc, i, integerType);
+    auto shifted = b.create<arith::ShLIOp>(loc, extOp, shiftAmount);
+    auto orOp = b.create<arith::OrIOp>(loc, integerType, result, shifted);
+    result = orOp.getResult();
+  }
+
+  return result;
 }
 
 }  // namespace heir
--- lib/Utils/TransformUtils.h
+++ lib/Utils/TransformUtils.h
@@ -4,13 +4,27 @@
 #include <string_view>
 
 #include "mlir/include/mlir/Dialect/Func/IR/FuncOps.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/Builders.h"              // from @llvm-project
 #include "mlir/include/mlir/IR/BuiltinOps.h"            // from @llvm-project
+#include "mlir/include/mlir/IR/Types.h"                 // from @llvm-project
+#include "mlir/include/mlir/IR/Value.h"                 // from @llvm-project
 
 namespace mlir {
 namespace heir {
 
 func::FuncOp detectEntryFunction(ModuleOp moduleOp,
                                  std::string_view entryFunction);
+
+/// Replace an integer-typed value with a memref of the individual bits. The
+/// lowest order bit of the integer is the first element in the memref.
+Value convertIntegerValueToMemrefOfBits(Value integer, OpBuilder &b,
+                                        Location loc);
+
+/// Replace a 1D memref of bits with an integer-typed value. The bits are
+/// interpreted so that the first element of the memref is the lowest order bit
+/// of the result integer.
+Value convertMemrefOfBitsToInteger(Value memref, Type resultType, OpBuilder &b,
+                                   Location loc);
 
 }  // namespace heir
 }  // namespace mlir
