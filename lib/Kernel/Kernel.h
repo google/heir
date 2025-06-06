fb5179128fe916d7e20b57be61e1b220da5f5d3f
Jeremy Kun <j2kun@users.noreply.github.com>
2025-06-06 15:28:57 -0700

    add a secret.kernel attribute

--- lib/Kernel/Kernel.h
+++ lib/Kernel/Kernel.h
@@ -1,8 +1,9 @@
 #ifndef LIB_KERNEL_KERNEL_H_
 #define LIB_KERNEL_KERNEL_H_
 
-#include "mlir/include/mlir/IR/Operation.h"         // from @llvm-project
-#include "mlir/include/mlir/IR/OperationSupport.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/DialectImplementation.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/Operation.h"              // from @llvm-project
+#include "mlir/include/mlir/IR/OperationSupport.h"       // from @llvm-project
 
 namespace mlir {
 namespace heir {
@@ -14,6 +15,33 @@ enum class KernelName {
 
 bool isSupportedKernel(Operation *op, KernelName name);
 
+inline raw_ostream &operator<<(raw_ostream &os,
+                               const heir::KernelName &kernelName) {
+  switch (kernelName) {
+    case heir::KernelName::MatvecNaive:
+      os << "MatvecNaive";
+    case heir::KernelName::MatvecDiagonal:
+      os << "MatvecDiagonal";
+    default:
+      os << "Unknown";
+  }
+  return os;
+}
+
 }  // namespace heir
+
+template <>
+struct FieldParser<heir::KernelName> {
+  static FailureOr<heir::KernelName> parse(AsmParser &parser) {
+    std::string kernelName;
+    if (parser.parseString(&kernelName)) return failure();
+
+    if (kernelName == "MatvecNaive") return heir::KernelName::MatvecNaive;
+    if (kernelName == "MatvecDiagonal") return heir::KernelName::MatvecDiagonal;
+
+    return failure();
+  }
+};
+
 }  // namespace mlir
 #endif  // LIB_KERNEL_KERNEL_H_
