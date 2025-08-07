70b00b89a99b6f4ff95b9c191247a4906945f885
Jeremy Kun <j2kun@users.noreply.github.com>
2025-08-07 13:21:44 -0700

    fin

--- lib/Utils/Layout/Codegen.cpp
+++ lib/Utils/Layout/Codegen.cpp
@@ -2,7 +2,9 @@
 
 #include "isl/ast_build.h"                                          // from @isl
 #include "isl/ast_type.h"                                           // from @isl
+#include "isl/constraint.h"                                         // from @isl
 #include "isl/ctx.h"                                                // from @isl
+#include "isl/map.h"                                                // from @isl
 #include "isl/set.h"                                                // from @isl
 #include "isl/space.h"                                              // from @isl
 #include "isl/union_map.h"                                          // from @isl
@@ -12,18 +14,134 @@
 namespace mlir {
 namespace heir {
 
-static __isl_give isl_union_map* convertRelationToString(
-    const presburger::IntegerRelation& rel, isl_ctx* ctx) {
-  // FIXME: replace with actual string construction
-  //  return isl_union_map_read_from_str(
-  //      ctx,
-  //      "{ S[row,col,ct,slot] -> [ct,slot] : "
-  //      "0 <= row,ct < 4 and 0 <= col < 8 and 0 <= slot < 32 and "
-  //      "((-row + slot) % 4) = 0 and (-col + ct + slot) % 8 = 0 }");
-  return isl_union_map_read_from_str(
-      ctx,
-      "{ S[d0,d1] -> [d1] : "
-      "0 <= d0 <= 10 and 0 <= d1 <= 10 and d0 - d1 = 0 }");
+using presburger::IntegerRelation;
+using presburger::VarKind;
+
+static __isl_give isl_union_map* convertRelationToUnionMap(
+    const IntegerRelation& rel, isl_ctx* ctx) {
+  // ISL has these variables types, which map to MLIR FPL variable types:
+  // isl_dim_param -> VarKind::Symbol
+  // isl_dim_in -> VarKind::Domain
+  // isl_dim_out -> VarKind::Range
+  // isl_dim_div -> VarKind::Local
+  //
+  // ISL puts parameters first, but since we have zero symbols, we can ignore
+  // the implied change to the offsets.
+  unsigned numDomain = rel.getNumVarKind(VarKind::Domain);
+  unsigned numRange = rel.getNumVarKind(VarKind::Range);
+  unsigned numLocal = rel.getNumVarKind(VarKind::Local);
+
+  // This represents the mapping from all four variables to just iterate over
+  // the range variables. This is specific to FHE because we know the packing
+  // must be a partial function from range to domain.
+  //
+  // E.g., this is specifying the `S[row,col,ct,slot] -> [ct,slot]` part of a
+  // union map like
+  //
+  //   {
+  //      S[row,col,ct,slot] -> [ct,slot] :
+  //      0 <= row,ct < 4
+  //      and 0 <= col < 8
+  //      and 0 <= slot < 32
+  //      and ((-row + slot) % 4) = 0 and (-col + ct + slot) % 8 = 0
+  //   }
+  //
+  unsigned numIn = numDomain + numRange;
+  unsigned numOut = numRange;
+  isl_space* space = isl_space_alloc(ctx, /*n_params=*/0, numIn, numOut);
+
+  // "S" is a hardcoded name for the inner-most statement that will be executed
+  // to assign the data entry to the ciphertext slot. Users will be forced to
+  // match on this name in the generated AST.
+  //
+  // E.g., S might be defined in plain C, using the layout example above, as
+  //
+  //   void S(int a, int b, int c, int d, int data[4][8], int slot[4][32]) {
+  //     slot[c][d] = data[a][b];
+  //   }
+  //
+  space = isl_space_set_tuple_name(space, isl_dim_in, "S");
+
+  // It would be helpful to set dim names, but we don't have access to them
+  // in the relation... Maybe add it?
+  //
+  // for (int i = 0; i < rel.getNumVars(); i++) {
+  //   isl_dim_type dimKind;
+  //   unsigned offset = 0;
+  //   if (rel.getVarKindAt(i) == VarKind::Domain) {
+  //     dimKind = isl_dim_in;
+  //     offset = rel.getVarKindOffset(VarKind::Domain);
+  //   } else if (rel.getVarKindAt(i) == VarKind::Range) {
+  //     dimKind = isl_dim_out;
+  //     offset = rel.getVarKindOffset(VarKind::Range);
+  //   } else if (rel.getVarKindAt(i) == VarKind::Local) {
+  //     dimKind = isl_dim_div;
+  //     offset = rel.getVarKindOffset(VarKind::Local);
+  //   }
+  //   char *name = "???";
+  //   space = isl_space_set_dim_name(space, dimKind, offset, name);
+  // }
+
+  isl_basic_map* bmap = isl_basic_map_universe(isl_space_copy(space));
+  isl_local_space* ls = isl_local_space_from_space(space);
+  if (numLocal > 0) {
+    ls = isl_local_space_add_dims(ls, isl_dim_div, numLocal);
+  }
+
+  // Identical variables in the range and domain must be linked
+  //
+  // E.g., in S[row,col,ct,slot] -> [ct,slot], the ct and slot variables are
+  // different and must be marked as the same.
+  for (int i = 0; i < numRange; i++) {
+    isl_constraint* c = isl_constraint_alloc_equality(isl_local_space_copy(ls));
+    c = isl_constraint_set_coefficient_si(c, isl_dim_in, numDomain + i, 1);
+    c = isl_constraint_set_coefficient_si(c, isl_dim_out, i, -1);
+    bmap = isl_basic_map_add_constraint(bmap, c);
+  }
+
+  // Now copy the coefficients of the constraints from the flattened IntegerSet
+  // to ISL.
+  auto copyConstraintsFromUnionMap = [&](bool isEquality) {
+    unsigned numConstraints =
+        isEquality ? rel.getNumEqualities() : rel.getNumInequalities();
+
+    for (unsigned idx = 0; idx < numConstraints; ++idx) {
+      SmallVector<int64_t> coeffs =
+          isEquality ? rel.getEquality64(idx) : rel.getInequality64(idx);
+
+      auto* spaceCopy = isl_local_space_copy(ls);
+      isl_constraint* c = isEquality
+                              ? isl_constraint_alloc_equality(spaceCopy)
+                              : isl_constraint_alloc_inequality(spaceCopy);
+
+      c = isl_constraint_set_constant_si(c, coeffs.back());
+
+      for (int i = 0; i < numDomain; i++) {
+        unsigned offset = i + rel.getVarKindOffset(VarKind::Domain);
+        c = isl_constraint_set_coefficient_si(c, isl_dim_in, i, coeffs[offset]);
+      }
+
+      for (int i = 0; i < numRange; i++) {
+        unsigned offset = i + rel.getVarKindOffset(VarKind::Range);
+        c = isl_constraint_set_coefficient_si(c, isl_dim_in, numDomain + i,
+                                              coeffs[offset]);
+      }
+
+      for (int i = 0; i < numLocal; i++) {
+        unsigned offset = i + rel.getVarKindOffset(VarKind::Local);
+        c = isl_constraint_set_coefficient_si(c, isl_dim_div, i,
+                                              coeffs[offset]);
+      }
+
+      bmap = isl_basic_map_add_constraint(bmap, c);
+    }
+  };
+
+  // Use it:
+  copyConstraintsFromUnionMap(/*isEquality=*/true);
+  copyConstraintsFromUnionMap(/*isEquality=*/false);
+
+  return isl_union_map_from_basic_map(bmap);
 }
 
 __isl_give isl_ast_node* constructAst(const presburger::IntegerRelation& rel,
@@ -32,7 +150,7 @@ __isl_give isl_ast_node* constructAst(const presburger::IntegerRelation& rel,
   // actually to write the ISL union-set as a string. This is because ISL's API
   // otherwise manually requires you to flatten constraints and remove
   // divs/mods.
-  isl_union_map* schedule = convertRelationToString(rel, ctx);
+  isl_union_map* schedule = convertRelationToUnionMap(rel, ctx);
 
   // Context and options are intentionally empty. We don't need any of these
   // features, though I admit I have not looked into what they can provide.
--- lib/Utils/Layout/CodegenTest.cpp
+++ lib/Utils/Layout/CodegenTest.cpp
@@ -11,123 +11,77 @@ namespace {
 using presburger::IntegerRelation;
 using ::testing::Eq;
 
+FailureOr<std::string> runAndGetCStr(const IntegerRelation &relation) {
+  isl_ctx *ctx = isl_ctx_alloc();
+  auto result = generateLoopNest(relation, ctx);
+  if (failed(result)) {
+    isl_ctx_free(ctx);
+    return failure();
+  }
+  isl_ast_node *tree = result.value();
+  std::string actual = std::string(isl_ast_node_to_C_str(tree));
+  isl_ast_node_free(tree);
+  isl_ctx_free(ctx);
+  // Add a leading newline for ease of comparison with multiline strings.
+  return "\n" + actual;
+}
+
 TEST(CodegenTest, PureAffineEquality) {
   MLIRContext context;
   IntegerRelation relation = relationFromString(
       "(d0, d1) : (d0 - d1 == 0, d0 >= 0, d1 >= 0, 10 >= d0, 10 >= d1)", 1,
       &context);
-  isl_ctx *ctx = isl_ctx_alloc();
-  auto result = generateLoopNest(relation, ctx);
+  auto result = runAndGetCStr(relation);
   ASSERT_TRUE(succeeded(result));
-  isl_ast_node *tree = result.value();
-  std::string actual = std::string(isl_ast_node_to_C_str(tree));
+  std::string actual = result.value();
   std::string expected = R"(
 for (int c0 = 0; c0 <= 10; c0 += 1)
   S(c0, c0);
 )";
-  // Remove leading newline
-  expected.erase(0, 1);
   ASSERT_THAT(actual, Eq(expected));
-
-  isl_ast_node_free(tree);
-  isl_ctx_free(ctx);
 }
 
-// TEST(CodegenTest, EqualityWithMod) {
-//   MLIRContext context;
-//   IntegerRelation relation = relationFromString(
-//       "(d0, d1) : ((d0 - d1) mod 2 == 0, d0 >= 0, d1 >= 0, 10 >= d0, 30 >=
-//       d1)", 1, &context);
-//   std::string dataId = "data";
-//   std::string slotId = "slot";
-//   relation.resetIds();
-//   relation.setId(VarKind::Domain, 0, Identifier(&dataId));
-//   relation.setId(VarKind::Range, 0, Identifier(&slotId));
-//
-//   auto loopNestRes = generateLoopNest(relation, &context);
-//   ASSERT_TRUE(succeeded(loopNestRes));
-//   auto actual = loopNestRes.value();
-//
-//   OpBuilder b(&context);
-//   auto d0 = b.getAffineDimExpr(0);
-//   auto d1 = b.getAffineDimExpr(1);
-//
-//   LoopNest expected;
-//   expected.addLoop(1, 0, 30);                 // d1
-//   Loop *loopD1 = expected.addLoop(0, 0, 10);  // d0
-//   loopD1->eliminatedVariables[2] = (d0 - d1).floorDiv(2);
-//
-//   loopD1->constraints.push_back((d0 - d1) % 2);
-//   loopD1->eq.push_back(true);
-//
-//   // The two constraints below should be redundant given d0 - d1 mod 2 above,
-//   // but I'm not sure how to detect that systematically in the
-//   implementation. loopD1->constraints.push_back(d1 + ((d0 - d1).floorDiv(2))
-//   * 2); loopD1->eq.push_back(false);
-//
-//   loopD1->constraints.push_back(-d1 - ((d0 - d1).floorDiv(2)) * 2 + 10);
-//   loopD1->eq.push_back(false);
-//
-//   ASSERT_THAT(actual, Eq(expected));
-// }
-//
-// TEST(CodegenTest, HaleviShoup) {
-//   MLIRContext context;
-//   // Data is 32x64, being packed into ciphertexts of size 1024 via
-//   Halevi-Shoup
-//   // diagonal layout.
-//   IntegerRelation relation = relationFromString(
-//       "(row, col, ct, slot) : ((slot - row) mod 32 == 0, (ct + slot - col)
-//       mod " "64 == 0, row >= 0, col >= 0, ct >= 0, slot >= 0, 1023 >= slot,
-//       31 >= " "ct, 31 >= row, 63 >= col)", 2, &context);
-//   auto loopNestRes = generateLoopNest(relation, &context);
-//   ASSERT_TRUE(succeeded(loopNestRes));
-//   auto actual = loopNestRes.value();
-//
-//   auto row = getAffineDimExpr(0, &context);
-//   auto col = getAffineDimExpr(1, &context);
-//   auto ct = getAffineDimExpr(2, &context);
-//   auto slot = getAffineDimExpr(3, &context);
-//
-//   LoopNest expected;
-//   expected.addLoop(2, 0, 31);             // ct
-//   expected.addLoop(3, 0, 1023);           // slot
-//   Loop *l3 = expected.addLoop(0, 0, 31);  // row
-//   Loop *l4 = expected.addLoop(1, 0, 63);  // col
-//
-//   l3->eliminatedVariables[4] = (-row + slot).floorDiv(32);
-//   l4->eliminatedVariables[5] = (-col + ct + slot).floorDiv(64);
-//
-//   l4->constraints = {
-//       (-row + slot) % 32,
-//       (-col + ct + slot) % 64,
-//       slot - ((-row + slot).floorDiv(32)) * 32,
-//       -slot + ((-row + slot).floorDiv(32)) * 32 + 31,
-//       col - ct - slot + ((-col + ct + slot).floorDiv(64) * 64 + 63),
-//       (-col + ct + slot) % 64};
-//   l4->eq = {true, true, false, false, false, false};
-//
-//   ASSERT_THAT(actual, Eq(expected));
-//
-//   // For some reason the AffineExpr equality operator doesn't work
-//   // because the first half-byte is off. Sometimes applying
-//   simplifyAffineExpr
-//   // fixes it, but not always. So constraints are compared manually here.
-//   ASSERT_THAT(actual.loops.size(), Eq(4));
-//   for (unsigned i = 0; i < actual.loops.size(); ++i) {
-//     for (const auto &[c1, c2] : llvm::zip(actual.loops[i].constraints,
-//                                           expected.loops[i].constraints)) {
-//       // llvm::errs() << "Comparing constraints: " << c1 << " vs " << c2 << "
-//       ?
-//       // "
-//       //              << (simplifyAffineExpr(c1, 4, 0) ==
-//       //                  simplifyAffineExpr(c2, 4, 0))
-//       //              << "\n";
-//       ASSERT_TRUE(simplifyAffineExpr(c1, 4, 0) == simplifyAffineExpr(c2, 4,
-//       0));
-//     }
-//   }
-// }
+TEST(CodegenTest, EqualityWithMod) {
+  MLIRContext context;
+  IntegerRelation relation = relationFromString(
+      "(d0, d1) : ((d0 - d1) mod 2 == 0, d0 >= 0, d1 >= 0, 10 >= d0, 30 >= d1)",
+      1, &context);
+  // std::string dataId = "data";
+  // std::string slotId = "slot";
+  // relation.resetIds();
+  // relation.setId(VarKind::Domain, 0, Identifier(&dataId));
+  // relation.setId(VarKind::Range, 0, Identifier(&slotId));
+
+  auto result = runAndGetCStr(relation);
+  ASSERT_TRUE(succeeded(result));
+  std::string actual = result.value();
+  std::string expected = R"(
+for (int c0 = 0; c0 <= 30; c0 += 1)
+  for (int c1 = -((c0 + 1) % 2) + 1; c1 <= 10; c1 += 2)
+    S(c1, c0);
+)";
+  ASSERT_THAT(actual, Eq(expected));
+}
+
+TEST(CodegenTest, HaleviShoup) {
+  MLIRContext context;
+  // Data is 32x64, being packed into ciphertexts of size 1024 via Halevi-Shoup
+  // diagonal layout.
+  IntegerRelation relation = relationFromString(
+      "(row, col, ct, slot) : ((slot - row) mod 32 == 0, (ct + slot - col) mod "
+      "64 == 0, row >= 0, col >= 0, ct >= 0, slot >= 0, 1023 >= slot, 31 >= "
+      "ct, 31 >= row, 63 >= col)",
+      2, &context);
+  auto result = runAndGetCStr(relation);
+  ASSERT_TRUE(succeeded(result));
+  std::string actual = result.value();
+  std::string expected = R"(
+for (int c0 = 0; c0 <= 31; c0 += 1)
+  for (int c1 = 0; c1 <= 1023; c1 += 1)
+    S(c1 % 32, -((-c0 - c1 + 1087) % 64) + 63, c0, c1);
+)";
+  ASSERT_THAT(actual, Eq(expected));
+}
 
 }  // namespace
 }  // namespace heir
