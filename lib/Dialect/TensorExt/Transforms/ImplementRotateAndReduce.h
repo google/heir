b256e7aeeed434b2fe754553703dc3222969c87f
Jeremy Kun <j2kun@users.noreply.github.com>
2025-08-21 14:42:34 -0700

    rebase on #2124

---
+++ lib/Dialect/TensorExt/Transforms/ImplementRotateAndReduce.h
@@ -0,0 +1,20 @@
+#ifndef LIB_DIALECT_TENSOREXT_TRANSFORMS_IMPLEMENTROTATEANDREDUCE_H_
+#define LIB_DIALECT_TENSOREXT_TRANSFORMS_IMPLEMENTROTATEANDREDUCE_H_
+
+#include "lib/Dialect/TensorExt/IR/TensorExtOps.h"
+#include "mlir/include/mlir/Dialect/Arith/IR/Arith.h"    // from @llvm-project
+#include "mlir/include/mlir/Dialect/Tensor/IR/Tensor.h"  // from @llvm-project
+#include "mlir/include/mlir/Pass/Pass.h"                 // from @llvm-project
+
+namespace mlir {
+namespace heir {
+namespace tensor_ext {
+
+#define GEN_PASS_DECL_IMPLEMENTROTATEANDREDUCE
+#include "lib/Dialect/TensorExt/Transforms/Passes.h.inc"
+
+}  // namespace tensor_ext
+}  // namespace heir
+}  // namespace mlir
+
+#endif  // LIB_DIALECT_TENSOREXT_TRANSFORMS_IMPLEMENTROTATEANDREDUCE_H_
