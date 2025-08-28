16d9fb32b0c22a12b9ba18bab8a58d3e3483af81
Jeremy Kun <j2kun@users.noreply.github.com>
2025-08-27 20:06:29 -0700

    show what skipping the dedicated partition op would look like

--- lib/Dialect/Polynomial/Conversions/PolynomialToModArith/PolynomialToModArith.cpp
+++ lib/Dialect/Polynomial/Conversions/PolynomialToModArith/PolynomialToModArith.cpp
@@ -1246,6 +1246,67 @@ struct ConvertINTT : public OpConversionPattern<INTTOp> {
   }
 };
 
+struct ConvertKeySwitchInner
+    : public OpConversionPattern<polynomial::KeySwitchInnerOp> {
+  ConvertKeySwitchInner(mlir::MLIRContext* context)
+      : OpConversionPattern<polynomial::KeySwitchInnerOp>(context) {}
+
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult matchAndRewrite(
+      polynomial::KeySwitchInnerOp op, OpAdaptor adaptor,
+      ConversionPatternRewriter& rewriter) const override {
+    auto b = ImplicitLocOpBuilder(op.getLoc(), rewriter);
+
+    RankedTensorType coefficientTensorType =
+        cast<RankedTensorType>(adaptor.getValue().getType());
+
+    if (coefficientTensorType.getRank() != 1) {
+      return rewriter.notifyMatchFailure(
+          op,
+          "Can only lower key_switch_inner when input is a single polynomial");
+    }
+
+    int64_t partSize = 7;  // FIXME: how to select this?
+    int64_t numPieces = coefficientTensorType.getShape()[0] / partSize;
+    int64_t sliceSize = partSize * numPieces;
+    int64_t extraPartSize = coefficientTensorType.getShape()[0] - sliceSize;
+
+    // Step 1: partition the coefficients of the polynomial into pieces. Note
+    // the partition may not be even, so there may be one leftover part of an
+    // uneven size that must be tracked manually through the rest of the
+    // lowering.
+
+    // First extract the parts that are divisible and reshape it i.e., a
+    // tensor<17xi32> with partitionSize 5 would first slice-extract to
+    // tensor<15xi32> then reshape to tensor<3x5xi32>.
+    SmallVector<OpFoldResult> offsets(1, b.getIndexAttr(0));
+    SmallVector<OpFoldResult> sizes(1, b.getIndexAttr(sliceSize));
+    SmallVector<OpFoldResult> strides(1, b.getIndexAttr(1));
+    Value extracted = tensor::ExtractSliceOp::create(b, adaptor.getValue(),
+                                                     offsets, sizes, strides);
+
+    SmallVector<int64_t> partitionShape = {numPieces, partSize};
+    Value reshaped = tensor::ReshapeOp::create(
+        b,
+        RankedTensorType::get(partitionShape,
+                              coefficientTensorType.getElementType()),
+        extracted);
+
+    // Then extract the trailing piece that doesn't have the same tensor size.
+    SmallVector<OpFoldResult> extraPartOffsets(1, b.getIndexAttr(sliceSize));
+    SmallVector<OpFoldResult> extraPartSizes(1, b.getIndexAttr(extraPartSize));
+    SmallVector<OpFoldResult> extraPartStrides(1, b.getIndexAttr(1));
+    Value extraPart =
+        tensor::ExtractSliceOp::create(b, adaptor.getValue(), extraPartOffsets,
+                                       extraPartSizes, extraPartStrides);
+
+    // TODO(#2157): implement the rest
+
+    return success();
+  }
+};
+
 void PolynomialToModArith::runOnOperation() {
   MLIRContext* context = &getContext();
 
@@ -1293,8 +1354,8 @@ void PolynomialToModArith::runOnOperation() {
                ConvertPolyBinop<AddOp, arith::AddIOp, mod_arith::AddOp>,
                ConvertPolyBinop<SubOp, arith::SubIOp, mod_arith::SubOp>,
                ConvertLeadingTerm, ConvertMonomial, ConvertMonicMonomialMul,
-               ConvertConstant, ConvertMulScalar, ConvertNTT, ConvertINTT>(
-      typeConverter, context);
+               ConvertConstant, ConvertMulScalar, ConvertNTT, ConvertINTT,
+               ConvertKeySwitchInner>(typeConverter, context);
   patterns.add<ConvertMul>(typeConverter, patterns.getContext(), getDivmodOp);
   addStructuralConversionPatterns(typeConverter, patterns, target);
   addTensorOfTensorConversionPatterns(typeConverter, patterns, target);
