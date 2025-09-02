79b31c18da2a03578dc8908b5515ee21a306eabf
Jeremy Kun <j2kun@users.noreply.github.com>
2025-09-02 15:00:43 -0700

    uniquify generated func names to avoid conflicts

---
+++ frontend/unique_name_test.py
@@ -0,0 +1,26 @@
+from absl.testing import absltest
+
+from heir import compile
+from heir.mlir import Secret, I32
+
+
+class UniqueNameTest(absltest.TestCase):
+  """This test ensures there are no name conflicts across lexical scopes."""
+
+  def test_one(self):
+    @compile()
+    def add(x: Secret[I32], y: Secret[I32]):
+      return x + y
+
+    self.assertEqual(add(1, 2), 3)
+
+  def test_two(self):
+    @compile()
+    def add(x: Secret[I32], y: Secret[I32]):
+      return x * y
+
+    self.assertEqual(add(2, 3), 6)
+
+
+if __name__ == "__main__":
+  absltest.main()
