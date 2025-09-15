6269158b0b51c9a647cbdee66e449407003d6b66
Jeremy Kun <jkun@google.com>
2025-09-15 10:31:35 -0700

    Fix one missed API update
    PiperOrigin-RevId: 807293780

--- tests/Dialect/ModArith/Conversions/mod_arith_to_arith/runner/lower_mul_test.cc
+++ tests/Dialect/ModArith/Conversions/mod_arith_to_arith/runner/lower_mul_test.cc
@@ -10,17 +10,11 @@ void _mlir_ciface_test_lower_mul(StridedMemRefType<int32_t, 1>* result);
 
 TEST(LowerMulTest, CorrectOutput) {
   StridedMemRefType<int32_t, 1> result;
-  int32_t data[4];
-  result.data = data;
-  result.offset = 0;
-  result.sizes[0] = 4;
-  result.strides[0] = 1;
   _mlir_ciface_test_lower_mul(&result);
   ASSERT_EQ(result.sizes[0], 4);
   EXPECT_EQ(result.data[0], 1600);
   EXPECT_EQ(result.data[1], 4269);
   EXPECT_EQ(result.data[2], 2);
   EXPECT_EQ(result.data[3], 0);
-
   free(result.data);
 }
