cc1f468decd9f730dcc43a04819f4f9fd306b6d2
Jeremy Kun <j2kun@users.noreply.github.com>
2025-10-23 21:48:53 -0700

    checkpoint

--- lib/Dialect/Orion/Conversions/OrionToCKKS/IRMaterializingVisitor.cpp
+++ lib/Dialect/Orion/Conversions/OrionToCKKS/IRMaterializingVisitor.cpp
@@ -3,12 +3,9 @@
 #include <cmath>
 
 #include "lib/Dialect/CKKS/IR/CKKSOps.h"
-#include "lib/Utils/MathUtils.h"
-#include "lib/Utils/Utils.h"
+#include "lib/Dialect/TensorExt/IR/TensorExtOps.h"
 #include "mlir/include/mlir/Dialect/Tensor/IR/Tensor.h"  // from @llvm-project
-#include "mlir/include/mlir/IR/BuiltinAttributeInterfaces.h"  // from @llvm-project
 #include "mlir/include/mlir/IR/BuiltinAttributes.h"      // from @llvm-project
-#include "mlir/include/mlir/IR/BuiltinTypeInterfaces.h"  // from @llvm-project
 #include "mlir/include/mlir/IR/BuiltinTypes.h"           // from @llvm-project
 #include "mlir/include/mlir/IR/TypeUtilities.h"          // from @llvm-project
 #include "mlir/include/mlir/Support/LLVM.h"              // from @llvm-project
@@ -55,16 +52,45 @@ Value IRMaterializingVisitor::operator()(const MultiplyNode<SSAValue>& node) {
 
 Value IRMaterializingVisitor::operator()(const LeftRotateNode<SSAValue>& node) {
   Value operand = this->process(node.operand);
+  if (auto tensorType = dyn_cast<RankedTensorType>(operand.getType())) {
+    // The thing being rotated is a ciphertext-semantic tensor
+    Value shift = arith::ConstantIntOp::create(builder, node.shift, 64);
+    return builder.create<tensor_ext::RotateOp>(operand.getType(), operand,
+                                                shift);
+  }
+
   IntegerAttr shift = builder.getI64IntegerAttr(node.shift);
   return ckks::RotateOp::create(builder, operand.getType(), operand, shift);
 }
 
 Value IRMaterializingVisitor::operator()(const ExtractNode<SSAValue>& node) {
-  // Unlike the version of IRMaterializingVisitor that operates before the
-  // scheme level, here we are literally extracting one entry from a 1D tensor.
   Value operand = this->process(node.operand);
+  RankedTensorType tensorType = cast<RankedTensorType>(operand.getType());
   Value index = arith::ConstantIndexOp::create(builder, node.index);
-  return tensor::ExtractOp::create(builder, operand, index);
+  if (isa<lwe::LWECiphertextType>(getElementTypeOrSelf(tensorType))) {
+    return tensor::ExtractOp::create(builder, operand, index);
+  }
+
+  // Extracting 1 row of a matrix, so offset is 0 except for the row dim
+  SmallVector<OpFoldResult> offsets(tensorType.getRank(),
+                                    builder.getIndexAttr(0));
+  offsets[0] = index;
+
+  // Sizes are 1 in the row dim, and the full size in other dims
+  SmallVector<OpFoldResult> sizes;
+  sizes.push_back(builder.getIndexAttr(1));
+  for (int i = 1; i < tensorType.getRank(); ++i) {
+    sizes.push_back(builder.getIndexAttr(tensorType.getDimSize(i)));
+  }
+
+  // Strides are all 1
+  SmallVector<OpFoldResult> strides(tensorType.getRank(),
+                                    builder.getIndexAttr(1));
+
+  RankedTensorType extractedType = RankedTensorType::get(
+      tensorType.getShape().drop_front(), tensorType.getElementType());
+  return builder.create<tensor::ExtractSliceOp>(extractedType, operand, offsets,
+                                                sizes, strides);
 }
 
 }  // namespace orion
--- lib/Dialect/Orion/Conversions/OrionToCKKS/IRMaterializingVisitor.h
+++ lib/Dialect/Orion/Conversions/OrionToCKKS/IRMaterializingVisitor.h
@@ -1,23 +1,25 @@
 #ifndef LIB_DIALECT_ORION_CONVERSIONS_ORIONTOCKKS_IRMATERIALIZINGVISITOR_H_
 #define LIB_DIALECT_ORION_CONVERSIONS_ORIONTOCKKS_IRMATERIALIZINGVISITOR_H_
 
+#include "lib/Dialect/LWE/IR/LWEOps.h"
 #include "lib/Dialect/LWE/IR/LWETypes.h"
 #include "lib/Kernel/AbstractValue.h"
 #include "lib/Kernel/ArithmeticDag.h"
-#include "llvm/include/llvm/ADT/TypeSwitch.h"            // from @llvm-project
-#include "llvm/include/llvm/Support/ErrorHandling.h"     // from @llvm-project
-#include "mlir/include/mlir/IR/Builders.h"               // from @llvm-project
-#include "mlir/include/mlir/IR/BuiltinTypeInterfaces.h"  // from @llvm-project
-#include "mlir/include/mlir/IR/BuiltinTypes.h"           // from @llvm-project
-#include "mlir/include/mlir/IR/TypeUtilities.h"          // from @llvm-project
-#include "mlir/include/mlir/IR/Value.h"                  // from @llvm-project
-#include "mlir/include/mlir/Support/LLVM.h"              // from @llvm-project
+#include "llvm/include/llvm/ADT/TypeSwitch.h"    // from @llvm-project
+#include "mlir/include/mlir/IR/Builders.h"       // from @llvm-project
+#include "mlir/include/mlir/IR/TypeUtilities.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/Value.h"          // from @llvm-project
+#include "mlir/include/mlir/Support/LLVM.h"      // from @llvm-project
 
 namespace mlir {
 namespace heir {
 namespace orion {
 
-// Walks the arithmetic DAG and generates MLIR for it.
+// Walks the arithmetic DAG and generates MLIR for it. This materializer is
+// weird because some SSA values are tensors of ciphertexts while others are
+// ciphertext-semantic tensors of cleartexts. The former requires simple ops
+// like tensor.extract and ckks.rotate, while the latter requires
+// tensor.extract_slice and tensor_ext.rotate.
 class IRMaterializingVisitor
     : public kernel::CachingVisitor<kernel::SSAValue, Value> {
  public:
@@ -30,24 +32,49 @@ class IRMaterializingVisitor
   Value binop(const T& node) {
     Value lhs = this->process(node.left);
     Value rhs = this->process(node.right);
-    return TypeSwitch<Type, Value>(getElementTypeOrSelf(lhs.getType()))
+    return TypeSwitch<Type, Value>(lhs.getType())
         .template Case<lwe::LWECiphertextType>([&](auto ty) {
-          if (isa<lwe::LWEPlaintextType>(rhs.getType())) {
-            return CtPtOp::create(builder, lhs, rhs).getResult();
+          if (isa<RankedTensorType>(rhs.getType())) {
+            // FIXME: (here and below) set the right scaling factor for the
+            // result plaintextType. Needs to get passed through from the input
+            // op!
+            lwe::LWEPlaintextType ptTy = lwe::LWEPlaintextType::get(
+                builder.getContext(), ty.getApplicationData(),
+                ty.getPlaintextSpace());
+            auto encodedRhs = lwe::RLWEEncodeOp::create(
+                builder, ptTy, rhs, ty.getPlaintextSpace().getEncoding(),
+                ty.getPlaintextSpace().getRing());
+            return CtPtOp::create(builder, lhs, encodedRhs).getResult();
           }
 
-          assert(isa<lwe::LWECiphertextType>(rhs.getType()));
+          if (!isa<lwe::LWECiphertextType>(rhs.getType())) {
+            emitError(lhs.getLoc()) << "Unsupported types for binary operation "
+                                       "(lhs is ciphertext): \n\nlhs="
+                                    << lhs << "\n\nrhs=" << rhs << "\n";
+            return Value();
+          }
           return CtCtOp::create(builder, lhs, rhs).getResult();
         })
-        .template Case<lwe::LWEPlaintextType>([&](auto ty) {
-          // There are no plaintext-plaintext ops
-          assert(isa<lwe::LWECiphertextType>(rhs.getType()));
-          return CtPtOp::create(builder, lhs, rhs).getResult();
+        .template Case<RankedTensorType>([&](auto ty) {
+          auto ctTy = dyn_cast<lwe::LWECiphertextType>(rhs.getType());
+          if (!ctTy) {
+            emitError(lhs.getLoc()) << "Unsupported types for binary operation "
+                                       "(lhs is cleartext): \n\nlhs="
+                                    << lhs << "\n\nrhs=" << rhs << "\n";
+            return Value();
+          }
+          lwe::LWEPlaintextType ptTy = lwe::LWEPlaintextType::get(
+              builder.getContext(), ctTy.getApplicationData(),
+              ctTy.getPlaintextSpace());
+          auto encodedLhs = lwe::RLWEEncodeOp::create(
+              builder, ptTy, lhs, ctTy.getPlaintextSpace().getEncoding(),
+              ctTy.getPlaintextSpace().getRing());
+          return CtPtOp::create(builder, encodedLhs, rhs).getResult();
         })
         .Default([&](Type) {
           emitError(lhs.getLoc())
-              << "Unsupported types for binary operation: \n\nlhs="
-              << lhs.getType() << "\n\nrhs=" << lhs.getType() << "\n";
+              << "Unsupported types for binary operation: \n\nlhs=" << lhs
+              << "\n\nrhs=" << rhs << "\n";
           return Value();
         });
   }
