410f64037efff81dac41c1c342570e7543add817
Jeremy Kun <jkun@google.com>
2026-03-13 22:02:33 -0700

    fix inline-activations for multiple callers
    PiperOrigin-RevId: 883492967

--- lib/Transforms/InlineActivations/InlineActivations.cpp
+++ lib/Transforms/InlineActivations/InlineActivations.cpp
@@ -1,17 +1,18 @@
 #include "lib/Transforms/InlineActivations/InlineActivations.h"
 
-#include <map>
-#include <string>
+#include <cctype>
 
-#include "llvm/include/llvm/Support/Debug.h"  // from @llvm-project
-#include "mlir/include/mlir/Dialect/Affine/IR/AffineOps.h"  // from @llvm-project
+#include "llvm/include/llvm/ADT/STLExtras.h"             // from @llvm-project
+#include "llvm/include/llvm/Support/Debug.h"             // from @llvm-project
 #include "mlir/include/mlir/Dialect/Affine/LoopUtils.h"  // from @llvm-project
 #include "mlir/include/mlir/Dialect/Func/IR/FuncOps.h"   // from @llvm-project
 #include "mlir/include/mlir/IR/SymbolTable.h"            // from @llvm-project
 #include "mlir/include/mlir/IR/Visitors.h"               // from @llvm-project
+#include "mlir/include/mlir/Pass/PassManager.h"          // from @llvm-project
 #include "mlir/include/mlir/Support/LLVM.h"              // from @llvm-project
 #include "mlir/include/mlir/Transforms/Inliner.h"        // from @llvm-project
 #include "mlir/include/mlir/Transforms/InliningUtils.h"  // from @llvm-project
+#include "mlir/include/mlir/Transforms/Passes.h"         // from @llvm-project
 
 #define DEBUG_TYPE "inline-activations"
 
@@ -20,14 +21,6 @@ namespace heir {
 
 #define GEN_PASS_DEF_INLINEACTIVATIONS
 #include "lib/Transforms/InlineActivations/InlineActivations.h.inc"
-
-namespace {
-
-static std::map<std::string, bool> activationMap = {
-    {"relu", true},
-};
-
-}  // namespace
 
 struct InlineActivations : impl::InlineActivationsBase<InlineActivations> {
   using InlineActivationsBase::InlineActivationsBase;
@@ -49,7 +42,7 @@ struct InlineActivations : impl::InlineActivationsBase<InlineActivations> {
     for (auto caller : callers) {
       auto symbol = caller.getCalleeAttr();
       auto callee = symbolTable.lookup(symbol.getAttr());
-      auto funcOp = dyn_cast<func::FuncOp>(callee);
+      auto funcOp = dyn_cast_or_null<func::FuncOp>(callee);
       if (!funcOp) {
         LLVM_DEBUG(llvm::dbgs() << "Skipping " << symbol.getValue().str()
                                 << " because it is not a function\n");
@@ -63,7 +56,15 @@ struct InlineActivations : impl::InlineActivationsBase<InlineActivations> {
         continue;
       }
 
-      if (!activationMap.contains(symbol.getValue().str())) {
+      auto isReluWithDigits = [](StringRef name) {
+        if (name == "relu") return true;
+        if (name.starts_with("relu_")) {
+          return llvm::all_of(name.drop_front(5), ::isdigit);
+        }
+        return false;
+      };
+
+      if (!isReluWithDigits(symbol.getValue())) {
         LLVM_DEBUG(llvm::dbgs()
                    << "Skipping " << symbol.getValue().str()
                    << " because it is not an activation function\n");
@@ -83,12 +84,17 @@ struct InlineActivations : impl::InlineActivationsBase<InlineActivations> {
 
       LLVM_DEBUG(llvm::dbgs() << "- Inlining " << caller << "\n");
       if (failed(inlineCall(inliner, config.getCloneCallback(), caller, funcOp,
-                            funcOp.getCallableRegion())))
+                            funcOp.getCallableRegion()))) {
         caller.emitError("function call cannot be inlined");
-
+        return signalPassFailure();
+      }
       caller.erase();
-      if (funcOp.use_empty()) funcOp.erase();
     }
+
+    // After inlining, remove any now-dead functions
+    OpPassManager pipeline("builtin.module");
+    pipeline.addPass(createSymbolDCEPass());
+    (void)runPipeline(pipeline, getOperation());
   }
 };
 
