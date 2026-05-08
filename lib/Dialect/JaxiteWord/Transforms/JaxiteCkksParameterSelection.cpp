3662527cc0187baf10ffc160e293de33123a8b07
Jeremy Kun <j2kun@users.noreply.github.com>
2026-05-08 15:38:33 -0700

    convert 64-bit prime moduli from ckks params to 30-bit analogues

--- lib/Dialect/JaxiteWord/Transforms/JaxiteCkksParameterSelection.cpp
+++ lib/Dialect/JaxiteWord/Transforms/JaxiteCkksParameterSelection.cpp
@@ -1,8 +1,12 @@
 #include "lib/Dialect/JaxiteWord/Transforms/JaxiteCkksParameterSelection.h"
 
+#include "lib/Dialect/CKKS/IR/CKKSAttributes.h"
 #include "lib/Dialect/JaxiteWord/IR/JaxiteWordAttributes.h"
 #include "lib/Dialect/JaxiteWord/IR/JaxiteWordOps.h"
-#include "mlir/include/mlir/IR/BuiltinOps.h"  // from @llvm-project
+#include "lib/Parameters/RLWEParams.h"
+#include "llvm/include/llvm/ADT/APInt.h"           // from @llvm-project
+#include "llvm/include/llvm/Support/MathExtras.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinOps.h"       // from @llvm-project
 
 namespace mlir {
 namespace heir {
@@ -19,14 +23,58 @@ struct JaxiteCkksParameterSelection
     MLIRContext *context = &getContext();
     ModuleOp module = getOperation();
 
-    SmallVector<int64_t, 2> qTowers = {1, 2};
-    SmallVector<int64_t, 1> pTowers = {3};
+    auto schemeParamAttr = module->getAttrOfType<ckks::SchemeParamAttr>(
+        ckks::CKKSDialect::kSchemeParamAttrName);
+    if (!schemeParamAttr) {
+      module->emitOpError() << "Missing ckks.schemeParam attribute";
+      signalPassFailure();
+      return;
+    }
+
+    int logN = schemeParamAttr.getLogN();
+    int ringDim = 1 << logN;
+
+    auto Q = schemeParamAttr.getQ().asArrayRef();
+    auto P = schemeParamAttr.getP().asArrayRef();
+
+    int totalBitsQ = 0;
+    for (auto q : Q) {
+      totalBitsQ += llvm::APInt(64, q).getActiveBits();
+    }
+
+    int totalBitsP = 0;
+    for (auto p : P) {
+      totalBitsP += llvm::APInt(64, p).getActiveBits();
+    }
+
+    std::vector<int64_t> existingPrimes;
+    std::vector<int64_t> qTowers;
+    std::vector<int64_t> pTowers;
+
+    int bitsGeneratedQ = 0;
+    while (bitsGeneratedQ < totalBitsQ) {
+      int64_t prime = findPrime(30, ringDim, existingPrimes);
+      qTowers.push_back(prime);
+      existingPrimes.push_back(prime);
+      bitsGeneratedQ += 30;
+    }
+
+    int bitsGeneratedP = 0;
+    while (bitsGeneratedP < totalBitsP) {
+      int64_t prime = findPrime(30, ringDim, existingPrimes);
+      pTowers.push_back(prime);
+      existingPrimes.push_back(prime);
+      bitsGeneratedP += 30;
+    }
 
     auto qTowersAttr = DenseI64ArrayAttr::get(context, qTowers);
     auto pTowersAttr = DenseI64ArrayAttr::get(context, pTowers);
 
-    auto ckksParamsAttr = CkksParametersAttr::get(context, qTowersAttr,
-                                                  pTowersAttr, 4, 5, 6, 7, 8);
+    int dnum = computeDnum(Q.size() - 1);
+
+    // FIXME: Replace dummy values for r, c, composite_degree, and batch.
+    auto ckksParamsAttr = CkksParametersAttr::get(
+        context, qTowersAttr, pTowersAttr, 4, 5, dnum, 7, 8);
 
     module->setAttr("jaxiteword.ckks_params", ckksParamsAttr);
   }
--- tests/Dialect/JaxiteWord/Transforms/doctest.mlir
+++ tests/Dialect/JaxiteWord/Transforms/doctest.mlir
@@ -4,7 +4,16 @@
 !ct = !jaxiteword.ciphertext<2, 3, 4>
 !ml = !jaxiteword.modulus_list<65536, 1152921504606844513, 1152921504606844417>
 
-func.func @test_add(%ct1 : !ct, %ct2 : !ct, %modulus_list: !ml) -> !ct {
-  %out = jaxiteword.add %ct1, %ct2, %modulus_list: (!ct, !ct, !ml) -> !ct
-  return %out : !ct
+module attributes {
+  ckks.schemeParam = #ckks.scheme_param<
+    logN = 13,
+    Q = [36028797018652673],
+    P = [1152921504606994433],
+    logDefaultScale = 45
+  >
+} {
+  func.func @test_add(%ct1 : !ct, %ct2 : !ct, %modulus_list: !ml) -> !ct {
+    %out = jaxiteword.add %ct1, %ct2, %modulus_list: (!ct, !ct, !ml) -> !ct
+    return %out : !ct
+  }
 }
