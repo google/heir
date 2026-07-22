78493cc19a73ade7785823e826bde6098b9f86e5
Alexander Viand <alexander.viand@gmail.com>
2026-07-22 12:16:26 +0200

    materializeLayout: use Simplex::computeIntegerBounds instead of IntegerRelation::getConstantBound64

--- lib/Transforms/ConvertToCiphertextSemantics/TypeConversion.cpp
+++ lib/Transforms/ConvertToCiphertextSemantics/TypeConversion.cpp
@@ -5,10 +5,12 @@
 #include <optional>
 
 #include "lib/Dialect/TensorExt/IR/TensorExtAttributes.h"
+#include "llvm/include/llvm/ADT/DynamicAPInt.h"     // from @llvm-project
 #include "llvm/include/llvm/Support/Debug.h"        // from @llvm-project
 #include "llvm/include/llvm/Support/raw_ostream.h"  // from @llvm-project
 #include "mlir/include/mlir/Analysis/Presburger/IntegerRelation.h"  // from @llvm-project
 #include "mlir/include/mlir/Analysis/Presburger/PresburgerSpace.h"  // from @llvm-project
+#include "mlir/include/mlir/Analysis/Presburger/Simplex.h"  // from @llvm-project
 #include "mlir/include/mlir/IR/BuiltinTypes.h"   // from @llvm-project
 #include "mlir/include/mlir/IR/TypeUtilities.h"  // from @llvm-project
 #include "mlir/include/mlir/IR/Types.h"          // from @llvm-project
@@ -19,13 +21,13 @@
 namespace mlir {
 namespace heir {
 
-using presburger::BoundType;
 using presburger::IntegerRelation;
 using presburger::VarKind;
 using tensor_ext::LayoutAttr;
 
 Type materializeLayout(Type dataType, LayoutAttr attr, int ciphertextSize) {
   IntegerRelation rel = attr.getIntegerRelation();
+  presburger::Simplex simplex(rel);
   llvm::SmallVector<int64_t> ciphertextSemanticShape;
   unsigned rangeOffset = rel.getVarKindOffset(VarKind::Range);
   for (unsigned varPos = rangeOffset;
@@ -36,20 +38,17 @@ Type materializeLayout(Type dataType, LayoutAttr attr, int ciphertextSize) {
                    << varPos - rangeOffset << " (ct), layout=" << attr << "\n";
       llvm::dbgs().flush();
     });
-    std::optional<int64_t> dimBound =
-        rel.getConstantBound64(BoundType::UB, varPos);
-    LLVM_DEBUG({
-      llvm::dbgs() << "materializeLayout: upper bound for range dimension "
-                   << varPos - rangeOffset << " (ct) = ";
-      if (dimBound) {
-        llvm::dbgs() << *dimBound;
-      } else {
-        llvm::dbgs() << "none";
-      }
-      llvm::dbgs() << "\n";
-    });
-    assert(dimBound && "No upper bound found for range variable");
-    ciphertextSemanticShape.push_back(dimBound.value() +
+    llvm::SmallVector<llvm::DynamicAPInt> coeffs(rel.getNumVars() + 1,
+                                                 llvm::DynamicAPInt(0));
+    coeffs[varPos] = llvm::DynamicAPInt(1);
+    auto bounds = simplex.computeIntegerBounds(coeffs);
+    assert(bounds.second.isBounded() &&
+           "No upper bound found for range variable");
+    LLVM_DEBUG(llvm::dbgs()
+                   << "materializeLayout: upper bound for range dimension "
+                   << varPos - rangeOffset << " (ct) = " << *bounds.second
+                   << "\n\n";);
+    ciphertextSemanticShape.push_back(int64_t(*bounds.second) +
                                       1);  // +1 is because UB is inclusive
   }
   // Last dimension is always the slot size. The relation may enforce a tighter
