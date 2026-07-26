3e3f70c9985f9e1d62cee712a679b797410e23cd
Marton Moro <moro.marton@gmail.com>
2026-07-26 14:22:35 +0200

    add detectBackend

--- lib/Target/Poulpy/PoulpyEmitter.cpp
+++ lib/Target/Poulpy/PoulpyEmitter.cpp
@@ -4,8 +4,9 @@
 #include "lib/Dialect/Poulpy/IR/PoulpyOps.h"
 #include "lib/Dialect/Poulpy/IR/PoulpyTypes.h"
 #include "lib/Target/Poulpy/PoulpyTemplates.h"
-#include "llvm/include/llvm/ADT/TypeSwitch.h"          // from @llvm-project
-#include "llvm/include/llvm/Support/FormatVariadic.h"  // from @llvm-project
+#include "llvm/include/llvm/ADT/TypeSwitch.h"           // from @llvm-project
+#include "llvm/include/llvm/Support/FormatVariadic.h"   // from @llvm-project
+#include "mlir/include/mlir/Dialect/Func/IR/FuncOps.h"  // from @llvm-project
 #include "mlir/include/mlir/Tools/mlir-translate/Translation.h"  // from @llvm-project
 
 #define DEBUG_TYPE "poulpy-emitter"
@@ -14,6 +15,31 @@ namespace mlir {
 namespace heir {
 namespace poulpy {
 
+namespace {
+FailureOr<std::string> detectBackend(ModuleOp* op) {
+  std::optional<PoulpyBackend> found;
+  for (auto funcOp : op->getOps<func::FuncOp>()) {
+    // TODO(mmoro): also check result types for backend information
+    for (Type argType : funcOp.getArgumentTypes()) {
+      auto moduleType = dyn_cast<ModuleType>(argType);
+      if (!moduleType) continue;
+      PoulpyBackend backend = moduleType.getBackend();
+      if (found.has_value() && found != backend) {
+        return op->emitError("poulpy module contains multiple backends");
+      }
+      found = backend;
+    }
+  }
+  switch (found.value_or(PoulpyBackend::FFT64Ref)) {
+    case PoulpyBackend::FFT64Ref:
+      return std::string("FFT64Ref");
+    case PoulpyBackend::NTT4x30Ref:
+      return std::string("NTT4x30Ref");
+  }
+  llvm_unreachable("unhandled PoulpyBackend");
+}
+}  // namespace
+
 void registerToPoulpyTranslation() {
   TranslateFromMLIRRegistration reg(
       "emit-poulpy", "translate the poulpy dialect to Rust code for poulpy",
@@ -21,7 +47,7 @@ void registerToPoulpyTranslation() {
         return translateToPoulpy(op, output);
       },
       [](DialectRegistry& registry) {
-        registry.insert<poulpy::PoulpyDialect>();
+        registry.insert<func::FuncDialect, poulpy::PoulpyDialect>();
       });
 }
 
@@ -50,6 +76,12 @@ LogicalResult PoulpyEmitter::translate(Operation& op) {
 
 LogicalResult PoulpyEmitter::printOperation(ModuleOp moduleOp) {
   os << kModulePrelude << "\n";
+  auto backend = detectBackend(&moduleOp);
+  if (failed(backend)) {
+    moduleOp.emitOpError("Error while detecting backend");
+    return failure();
+  }
+  os << "type BE = " << backend.value() << ";\n";
   for (Operation& op : moduleOp) {
     if (failed(translate(op))) {
       return failure();
